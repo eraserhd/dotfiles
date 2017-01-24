268551fba7a980036f847b6e175261b2d4efbd92
Jason Felice <jason.m.felice@gmail.com>
2017-01-24 15:54:59 -0500

    Some Terminal.app settings

--- macos/install.sh
+++ macos/install.sh
@@ -7,3 +7,6 @@ defaults write com.apple.finder AppleShowAllFiles NO || exit $?
 # Why would I ever want a single-button mouse?
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+
+# Terminal.app
+defaults write com.apple.Terminal "Default Window Settings" Pro
