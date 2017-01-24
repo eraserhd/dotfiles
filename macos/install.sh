537a88fa41fd0ec7654d154c7b79af5f1b57795b
Jason Felice <jason.m.felice@gmail.com>
2017-01-24 15:43:14 -0500

    Enable right-click on magic mouse

--- macos/install.sh
+++ macos/install.sh
@@ -3,3 +3,7 @@
 [ "$(uname -s)" != "Darwin" ] || exit
 
 defaults write com.apple.finder AppleShowAllFiles NO || exit $?
+
+# Why would I ever want a single-button mouse?
+defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
