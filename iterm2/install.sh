b0c6472f513bb1a6748e6dd10a8de8bfe41d8d0a
Jason Felice <jason.m.felice@gmail.com>
2017-01-24 09:10:17 -0800

    Don't install iTerm2

--- iterm2/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ "$(uname -s)" == "Darwin" ] || exit
-
-set -e
-
-# Load settings from our config folder
-defaults write com.googlecode.iterm2 PrefsCustomFolder ~/.config/iterm2
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder 1
