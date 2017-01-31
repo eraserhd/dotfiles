5abb0e1376873b0e2802421da096faa3fa32af4f
Jason Felice <jason.m.felice@gmail.com>
2017-01-31 17:02:14 -0500

    Fix neovim plugins for Clojure

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 pip install neovim
+
+(
+  cd ~/.config/nvim/bundle/node-host/
+  npm install
+)
+
+# Neovim can't handle multiple plugins right now, so as a hack, we install a
+# manually generated rplugin.vim
+#/usr/local/bin/nvim --cmd 'au VimEnter * UpdateRemotePlugins' --cmd 'au VimEnter * q'
+mkdir -p ~/.local/share/nvim
+cp ./neovim/rplugin.vim ~/.local/share/nvim/
