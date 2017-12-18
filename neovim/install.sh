030e81d80e1b0757306f58fe94ef44a91763c620
Jason Felice <jason.m.felice@gmail.com>
2017-12-18 16:37:23 -0500

    Update neovim plugins

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
 pip install neovim
-
-(
-  cd ~/.config/nvim/bundle/node-host/
-  npm install
-)
-
-# Neovim can't handle multiple plugins right now, so as a hack, we install a
-# manually generated rplugin.vim
-#/usr/local/bin/nvim --cmd 'au VimEnter * UpdateRemotePlugins' --cmd 'au VimEnter * q'
-mkdir -p ~/.local/share/nvim
-cp ./neovim/rplugin.vim ~/.local/share/nvim/
+npm install -g neovim
 
 cabal install happy
 cabal install ghc-mod
@@ -19,5 +9,6 @@ cabal install ghc-mod
 # Not sure this works
 nvim -E -s <<EOF
 VimProcInstall
+UpdateRemotePlugins
 q
 EOF
