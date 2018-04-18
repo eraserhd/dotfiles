8a391bdc09cb603670c9fb7c3422434ec52eb44c
Jason Felice <jason.m.felice@gmail.com>
2018-04-18 11:59:07 -0400

    Indent

--- daily/src/main.rs
+++ daily/src/main.rs
@@ -1,5 +1,4 @@
-use std::process::Command;
-
+use std::process::Command; 
 const COMPLETE_SCRIPT : &'static str = r#"
 tell application "Things3"
   repeat with toDo in to dos of list "Today"
@@ -31,49 +30,34 @@ end tell
 "#;
 
 fn main() {
-    Command::new("osascript")
-        .arg("-e")
-        .arg(ANKI_SCRIPT)
+    Command::new("osascript").arg("-e").arg(ANKI_SCRIPT)
         .status()
         .expect("failed to run Anki script");
 
-    Command::new("open")
-        .arg("/System/Library/CoreServices/Menu Extras/Bluetooth.menu")
+    Command::new("open").arg("/System/Library/CoreServices/Menu Extras/Bluetooth.menu")
         .status()
         .expect("failed to open bluetooth menu");
 
-    Command::new("osascript")
-        .arg("-e")
-        .arg(COMPLETE_SCRIPT)
+    Command::new("osascript").arg("-e").arg(COMPLETE_SCRIPT)
         .status()
         .expect("failed to check off 'Run Daily' task");
 
-    Command::new("git")
-        .arg("add")
-        .arg("-A")
+    Command::new("git").arg("add").arg("-A")
         .current_dir("/Users/jfelice/src/data")
         .status()
         .expect("failed to `git add -A` in data");
 
-    Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg("daily")
+    Command::new("git").arg("commit").arg("-m").arg("daily")
         .current_dir("/Users/jfelice/src/data")
         .status()
         .expect("failed to `git commit` in data");
 
-    Command::new("git")
-        .arg("push")
-        .arg("origin")
-        .arg("master")
+    Command::new("git").arg("push").arg("origin").arg("master")
         .current_dir("/Users/jfelice/src/data")
         .status()
         .expect("failed to `git push` in data");
 
-    Command::new("gtypist")
-        .arg("-b")
-        .arg("/Users/jfelice/src/dotfiles/gtypist/gtypist.typ")
+    Command::new("gtypist").arg("-b").arg("/Users/jfelice/src/dotfiles/gtypist/gtypist.typ")
         .status()
         .expect("failed to `gtypist`");
 }
