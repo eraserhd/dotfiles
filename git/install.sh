f199f77ba5d6197c72093af6b271024334be9b20
Jason Felice <jason.m.felice@gmail.com>
2018-08-19 20:14:31 -0400

    Restore basic git repos

---
+++ git/install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -eo pipefail
+
+mkdir -p ~/src
+while read repo_url; do
+  dir=${repo_url##*/}  
+  dir=${dir%.git}
+  if [[ ! -d ~/src/"$dir" ]]; then
+    ( cd ~/src ; git clone "$repo_url" )
+  fi
+done <git/repos.txt
