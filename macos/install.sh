20379190fbdd258ee25279152579182ccfd57411
Jason Felice <jason.m.felice@gmail.com>
2018-08-21 09:56:49 -0400

    Set dock preferences better

--- macos/install.sh
+++ macos/install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-[ "$(uname -s)" != "Darwin" ] && exit
+set -e
+
+[ $(uname -s) != Darwin ] && exit
 
 defaults write com.apple.finder AppleShowAllFiles NO || exit $?
 
 # Dock
-defaults write com.apple.dock autohide -bool true
+osascript -e 'tell application "System Events" to set the autohide of the dock preferences to true'
 
 # Why would I ever want a single-button mouse?
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
-
-# Terminal.app
-defaults write com.apple.Terminal "Default Window Settings" Pro
-defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
 
 # No Command+Space for Spotlight
 # This doesn't seem to take effect immediately unless changed through the UI :(
