b6a7805a53b7f392afb20b6565a6b81b2b085a22
Jason Felice <jason.m.felice@gmail.com>
2019-02-22 09:50:36 -0500

    Inject select-nrepl middleware

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,1 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.21.0"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.21.0"]]
+        :dependencies [[net.eraserhead/select-nrepl "0.1.0-SNAPSHOT"]]
+        :repl-options {:nrepl-middleware [select-nrepl.core/wrap-select]}}}
