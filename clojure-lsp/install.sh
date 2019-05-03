65c58f7ec1f47b58fb9465784694452e15fedb5c
Jason Felice <jason.m.felice@gmail.com>
2019-05-03 13:04:51 -0400

    Not using clojure-lsp right now

--- clojure-lsp/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-release="20180913T061535"
-
-set -eo pipefail
-
-curl -L https://github.com/snoe/clojure-lsp/releases/download/release-${release}/clojure-lsp >/usr/local/bin/clojure-lsp
-chmod +x /usr/local/bin/clojure-lsp
