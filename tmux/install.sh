41f55de3342e990f428af9d0ff333137278f4a8b
Jason Felice <jason.m.felice@gmail.com>
2019-05-30 10:24:27 -0400

    Clean up tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+./tmux/tmux.symlink/plugins/tpm/bin/install_plugins
+
 tic tmux/tmux-256color.ncurses57.terminfo
-/usr/local/opt/ncurses/bin/tic tmux/tmux-256color.ncurses61.terminfo
+/usr/local/opt/ncurses/bin/tic tmux/tmux-256color.ncurses61.terminfo || true
+
