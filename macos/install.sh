5350f0632542891a8336ce4312b98b7b2081c825
Jason Felice <jason.m.felice@gmail.com>
2019-06-09 19:10:18 -0400

    Remove Terminal.app settings

--- macos/install.sh
+++ macos/install.sh
@@ -10,6 +10,3 @@ defaults write com.apple.dock autohide -bool true
 # Why would I ever want a single-button mouse?
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
-
-# Terminal.app
-defaults write com.apple.Terminal "Default Window Settings" Pro
