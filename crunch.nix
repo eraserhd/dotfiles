924bf723e595ab4464e1cda7ca619481d54a837f
Jason Felice <jason.m.felice@gmail.com>
2019-06-08 21:39:24 -0400

    Update crunch.eraserhead.net to point here

--- crunch.nix
+++ crunch.nix
@@ -3,6 +3,17 @@
 
 { config, pkgs, ... }:
 
+let updateDNSScript = pkgs.writeShellScriptBin "update-dns" ''
+  #!${pkgs.bash}/bin/bash
+
+  ${builtins.readFile ./bin/private.sh}
+
+  exec ${pkgs.curl}/bin/curl -s -X PUT -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" \
+    -d '{"data": "'"$(curl -s http://ipinfo.io/ip)"'"}' \
+    "https://api.digitalocean.com/v2/domains/eraserhead.net/records/73014284" \
+    >/dev/null
+''; in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -53,6 +64,12 @@
   documentation.dev.enable = true;
 
   services.openssh.enable = true;
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+        "*/5 * * * *    jfelice  ${updateDNSScript}/bin/update-dns"
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 
