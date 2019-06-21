ad4e9bc72920c830df662fddc4785d06d228f40e
Jason Felice <jason.m.felice@gmail.com>
2019-06-20 21:18:32 -0400

    Reinstall nix-darwin

---
+++ macbook.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/src/dotfiles/macbook.nix
+  environment.darwinConfig = "$HOME/src/dotfiles/macbook.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
+  # programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 1;
+  nix.buildCores = 4;
+}
