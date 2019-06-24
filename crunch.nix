1114c814ca052796d28151090ce873287b0de39c
Jason Felice <jason.m.felice@gmail.com>
2019-06-24 12:29:50 -0400

    Update crunch kernel

--- crunch.nix
+++ crunch.nix
@@ -23,6 +23,7 @@ let updateDNSScript = pkgs.writeShellScriptBin "update-dns" ''
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking = {
       hostName = "crunch";
