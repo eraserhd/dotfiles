82c797e80978a1dddf82eebe4070dda0c0da0220
Jason Felice <jason.m.felice@gmail.com>
2019-07-22 16:30:45 -0400

    Import nixpkgs as a module

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,4 @@
-import (fetchGit {
-  url = "git@github.com:eraserhd/nixpkgs.git";
-  ref = "eraserhd";
-  rev = "c05c72b0041165b60fa5df5d4f50c251dd4bcb86";
-})
-{
+import ../nixpkgs {
   overlays = [ (self: super: { 
     kakoune = super.kakoune.override {
       configure = {
