c64b3cc82f0717097bc0f5eae804950dace8cef6
Jason Felice <jason.m.felice@gmail.com>
2019-07-26 12:16:52 -0400

    Make :r rebuild darwin config

--- bash/default.nix
+++ bash/default.nix
@@ -4,6 +4,14 @@
   config = {
     programs.bash.interactiveShellInit = ''
       :r() {
+        if command -v darwin-rebuild >/dev/null; then
+          pushd ~/src/dotfiles >/dev/null
+          darwin-rebuild build || return $?
+          if [[ $(readlink /run/current-system) != $(readlink ./result) ]]; then
+            darwin-rebuild switch || return $?
+          fi
+          popd >/dev/null
+        fi
         unset __NIX_DARWIN_SET_ENVIRONMENT_DONE __ETC_BASHRC_SOURCED
         exec $SHELL -l
       }
