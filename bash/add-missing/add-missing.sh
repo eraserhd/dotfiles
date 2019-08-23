a8a58bfd102b8d51a18e4cd7fd018d31d311f122
Jason Felice <jason.m.felice@gmail.com>
2019-08-23 16:29:15 -0400

    add-missing: adds /result to .gitignore when it exists

--- bash/add-missing/add-missing.sh
+++ bash/add-missing/add-missing.sh
@@ -4,6 +4,6 @@ if ! [[ -f nixpkgs.nix ]]; then
     printf '<nixpkgs>\n' >nixpkgs.nix
 fi
 
-if ! [[ -f .gitignore ]]; then
-    printf '/result\n' >.gitignore
+if ! [[ -f .gitignore ]] || ! grep -q '^/result$' .gitignore; then
+    printf '/result\n' >>.gitignore
 fi
--- bash/add-missing/release.nix
+++ bash/add-missing/release.nix
@@ -4,11 +4,22 @@ let
   add-missing = pkgs.callPackage ./derivation.nix {};
 
 in rec {
-  testEmptyDir = pkgs.runCommandNoCC "add-missing-test" {} ''
-    mkdir -p $out
-    cd $out
+  test = pkgs.runCommandNoCC "add-missing-test" {} ''
+    set -x
+
+    mkdir -p $out/empty-dir
+    cd $out/empty-dir
     ${add-missing}/bin/add-missing
     grep -q '^<nixpkgs>$' nixpkgs.nix
     grep -q '^/result' .gitignore
+
+    mkdir -p $out/gitignore-no-result
+    cd $out/gitignore-no-result
+    printf '/foo\n' >.gitignore
+    ${add-missing}/bin/add-missing
+    grep -q '^/foo$' .gitignore
+    grep -q '^/result$' .gitignore
+
+    set +x
   '';
 }
