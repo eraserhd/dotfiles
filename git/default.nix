3df6f1a1b228c83e33a112248bf16fbc657ebe78
Jason Felice <jason.m.felice@gmail.com>
2019-09-03 02:08:27 -0400

    Manage .git/hub with home-manager

--- git/default.nix
+++ git/default.nix
@@ -8,6 +8,7 @@
     ];
 
     home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".config/hub".source = ./hub;
       home.file.".gitconfig".source = ./gitconfig;
       home.file.".gitignore_global".source = ./gitignore_global;
     };
