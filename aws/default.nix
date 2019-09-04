cbf669ef13291d7c5110a03ba352f152becdab93
Jason Felice <jason.m.felice@gmail.com>
2019-09-03 21:09:41 -0400

    Manage AWS dotfiles with home-manager

--- aws/default.nix
+++ aws/default.nix
@@ -5,5 +5,11 @@
     environment.systemPackages = with pkgs; [
       awscli
     ];
+
+    home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".boto".source = ./boto;
+      home.file.".aws/config".source = ./config;
+      home.file.".aws/credentials".source = ./credentials;
+    };
   };
 }
