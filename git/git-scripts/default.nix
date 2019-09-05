4c01254e0852540ebc92f3b2ab54a3efd0804ee4
Jason Felice <jason.m.felice@gmail.com>
2019-09-05 10:36:03 -0400

    local.git-scripts:Correctly parameterize hub

--- git/git-scripts/default.nix
+++ git/git-scripts/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, bash, git }:
+{ stdenv, bash, git, gitAndTools }:
 
 stdenv.mkDerivation {
   name = "local-git-scripts-2019.09.05";
@@ -6,11 +6,15 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp git-cleanup git-fork git-l $out/bin/
-    substituteInPlace $out/bin/git-l \
-      --subst-var-by bash '${bash}' \
-      --subst-var-by git '${git}'
     substituteInPlace $out/bin/git-cleanup \
       --subst-var-by bash '${bash}' \
       --subst-var-by git '${git}'
+    substituteInPlace $out/bin/git-l \
+      --subst-var-by bash '${bash}' \
+      --subst-var-by git '${git}'
+    substituteInPlace $out/bin/git-fork \
+      --subst-var-by bash '${bash}' \
+      --subst-var-by git '${git}' \
+      --subst-var-by hub '${gitAndTools.hub}'
   '';
 }
