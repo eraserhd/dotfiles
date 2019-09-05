1ccc656486a65bcca5e4d9abb500a7c2e12465db
Jason Felice <jason.m.felice@gmail.com>
2019-09-05 10:33:02 -0400

    local.git-scripts: correctly pass bash

--- git/git-scripts/default.nix
+++ git/git-scripts/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, git }:
+{ stdenv, bash, git }:
 
 stdenv.mkDerivation {
   name = "local-git-scripts-2019.09.05";
@@ -7,6 +7,10 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     cp git-cleanup git-fork git-l $out/bin/
     substituteInPlace $out/bin/git-l \
+      --subst-var-by bash '${bash}' \
+      --subst-var-by git '${git}'
+    substituteInPlace $out/bin/git-cleanup \
+      --subst-var-by bash '${bash}' \
       --subst-var-by git '${git}'
   '';
 }
