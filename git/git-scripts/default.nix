58b5cf6acbf09a7ede913aa2e3454e18cb5048c8
Jason Felice <jason.m.felice@gmail.com>
2019-09-05 10:28:24 -0400

    local.git-scripts: Use pure git

--- git/git-scripts/default.nix
+++ git/git-scripts/default.nix
@@ -1,4 +1,4 @@
-{ stdenv }:
+{ stdenv, git }:
 
 stdenv.mkDerivation {
   name = "local-git-scripts-2019.09.05";
@@ -6,5 +6,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp git-cleanup git-fork git-l $out/bin/
+    substituteInPlace $out/bin/git-l \
+      --subst-var-by git '${git}'
   '';
 }
