132ba4963518c104610dddfda628239f23192ed0
Jason Felice <jason.m.felice@gmail.com>
2019-09-05 11:14:39 -0400

    Set home-manager verbose

--- crunch.nix
+++ crunch.nix
@@ -94,6 +94,8 @@ let updateDNSScript = pkgs.writeShellScriptBin "update-dns" ''
     root.openssh.authorizedKeys.keys = allowedKeys;
   };
 
+  home-manager.verbose = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
