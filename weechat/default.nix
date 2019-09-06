bd18e5fad9099c588cb3289deac2cdc256b80d55
Jason Felice <jason.m.felice@gmail.com>
2019-09-06 15:32:37 -0400

    Manage weechat config with home-manager

--- weechat/default.nix
+++ weechat/default.nix
@@ -12,8 +12,33 @@
         });
       })
     ];
+
     environment.systemPackages = with pkgs; [
       weechat
     ];
+
+    home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".weechat/alias.conf".source = ./config/alias.conf;
+      home.file.".weechat/aspell.conf".source = ./config/aspell.conf;
+      home.file.".weechat/buflist.conf".source = ./config/buflist.conf;
+      home.file.".weechat/charset.conf".source = ./config/charset.conf;
+      home.file.".weechat/exec.conf".source = ./config/exec.conf;
+      home.file.".weechat/fifo.conf".source = ./config/fifo.conf;
+      home.file.".weechat/fset.conf".source = ./config/fset.conf;
+      home.file.".weechat/irc.conf".source = ./config/irc.conf;
+      home.file.".weechat/logger.conf".source = ./config/logger.conf;
+      home.file.".weechat/plugins.conf".source = ./config/plugins.conf;
+      home.file.".weechat/python.conf".source = ./config/python.conf;
+      home.file.".weechat/python/autoload/url_hint.py".source = ./config/python/autoload/url_hint.py;
+      home.file.".weechat/relay.conf".source = ./config/relay.conf;
+      home.file.".weechat/script.conf".source = ./config/script.conf;
+      home.file.".weechat/sec.conf".source = ./config/sec.conf;
+      home.file.".weechat/spell.conf".source = ./config/spell.conf;
+      home.file.".weechat/tcl.conf".source = ./config/tcl.conf;
+      home.file.".weechat/trigger.conf".source = ./config/trigger.conf;
+      home.file.".weechat/weechat.conf".source = ./config/weechat.conf;
+      home.file.".weechat/weemoji.json".source = ./config/weemoji.json;
+      home.file.".weechat/xfer.conf".source = ./config/xfer.conf;
+    };
   };
 }
