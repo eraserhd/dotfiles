fe87f8537f2eccf0f30a189cfe56bcc2519fecd3
Jason Felice <jason.m.felice@gmail.com>
2019-09-17 22:28:14 -0400

    Start osxsnarf with launchd

--- plan9/osxsnarf.nix
+++ plan9/osxsnarf.nix
@@ -6,5 +6,15 @@ let
 in {
   config = mkIf (cfg.terminal.enable && pkgs.stdenv.isDarwin) {
     environment.systemPackages = [ pkgs.osxsnarf ];
+
+    launchd.agents.osxsnarf = {
+      command = "${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace " +
+        "${pkgs.plan9port}/bin/9 ${pkgs.osxsnarf}/bin/osxsnarf -f " +
+        "'unix!/Users/jfelice/.run/plan9/srv/snarf'";
+      serviceConfig = {
+        KeepAlive = true;
+        UserName = "jfelice";
+      };
+    };
   };
 }
