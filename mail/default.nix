fc26e2cfd72c1dd66caa9822810370afee4436be
Jason Felice <jason.m.felice@gmail.com>
2019-09-28 01:16:15 -0400

    Make module work on nix-darwin

--- mail/default.nix
+++ mail/default.nix
@@ -1,10 +1,12 @@
-{ lib, config, ... }:
+{ lib, config, options, ... }:
 
 with lib;
 {
   options.local.sendOutgoingMailWithSES = mkEnableOption "SES";
 
-  config = mkIf config.local.sendOutgoingMailWithSES {
+  config = mkIf config.local.sendOutgoingMailWithSES
+  (if (builtins.hasAttr "defaultMailServer" options.networking)
+  then {
     networking.defaultMailServer = {
       hostName = "email-smtp.us-west-2.amazonaws.com:587";
       directDelivery = true;
@@ -13,5 +15,7 @@ with lib;
       useSTARTTLS = true;
       root = "jason.m.felice@gmail.com";
     } // import ./ses-user.nix;
-  };
+  }
+  else {
+  });
 }
