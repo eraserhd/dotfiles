2f8a1beab7ecaa5975430a0c31b687ba9647e72f
Jason Felice <jason.m.felice@gmail.com>
2019-10-01 11:28:17 -0400

    Enable crashdumps on crunch

--- crunch.nix
+++ crunch.nix
@@ -11,6 +11,8 @@
       /etc/nixos/hardware-configuration.nix
     ];
 
+  boot.crashDump.enable = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
