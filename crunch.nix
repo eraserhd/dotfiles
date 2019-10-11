3ea2572387ee6ff43e65ee7b6b0f324b647ddee0
Jason Felice <jason.m.felice@gmail.com>
2019-10-11 09:29:07 -0400

    Comment out for now

--- crunch.nix
+++ crunch.nix
@@ -86,7 +86,7 @@
   # should.
   system.stateVersion = "19.03";
 
-  local.nagiosServer.enable = true;
+  #local.nagiosServer.enable = true;
   local.plan9.cpu.enable = true;
   local.sendOutgoingMailWithSES.enable = true;
   local.updateDNS.enable = true;
