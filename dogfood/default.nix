7de47b82cae0412f063ce95240baf4ef002a5ec9
Jason Felice <jason.m.felice@gmail.com>
2019-10-18 15:02:17 -0400

    Add kak-plumb to dogfood

--- dogfood/default.nix
+++ dogfood/default.nix
@@ -8,6 +8,12 @@
           src = super.pkgs.fetchFromGitHub (import ./kakoune.nix);
         });
 
+        kakounePlugins = super.kakounePlugins // {
+          kak-plumb = super.kakounePlugins.kak-plumb.overrideAttrs (oldAttrs: rec {
+            src = super.pkgs.fetchFromGitHub (import ./kak-plumb.nix);
+          });
+        };
+
         parinfer-rust = super.parinfer-rust.overrideAttrs (oldAttrs: rec {
           # Remove once we get 0.4.x merged to nixpkgs
           buildInputs = [ self.llvmPackages.libclang self.llvmPackages.clang ];
---
+++ dogfood/kak-plumb.nix
@@ -0,0 +1,6 @@
+{
+  owner = "eraserhd";
+  repo = "kak-plumb";
+  rev = "c06707f58554f06ca9b60ae7c726e3de1dcceddb";
+  sha256 = "133fh1k5wgf9sqhww3h6rbw115jxsfw2432q66mrgfiqswyy39b8";
+}
