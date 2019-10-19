e5e22f7484e2a43a67e8e8c5f431db0ce3bcfe48
Jason Felice <jason.m.felice@gmail.com>
2019-10-19 14:43:35 -0400

    Bump tmux version

--- dogfood/tmux.nix
+++ dogfood/tmux.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "tmux";
-  rev = "291252a600cb0d211b4668b8d9e3febdebed80b4";
-  sha256 = "1givy6shra3rhniaxxgar8ramgkl64bvvlxwc9jsqf6bwycb07aq";
+  rev = "bed3c0ac1ee8bb30332ee32e20abf64924d3dfac";
+  sha256 = "1mzfbi9bp01yg7fds0rdja49zsqsi0kk9ka89zlh3xxgf1zdlmch";
 }
