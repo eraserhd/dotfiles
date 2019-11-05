1edd4e072e218cfcb0b963230750d8b2d95977da
Jason Felice <jason.m.felice@gmail.com>
2019-11-04 22:29:49 -0500

    Allow 9.sh to be used in sandbox

--- networking/plan9/wrapper/9.sh
+++ networking/plan9/wrapper/9.sh
@@ -1,10 +1,8 @@
 #!@bash@/bin/bash
 
-set -e
-
 : ${XDG_RUNTIME_DIR:=~/.run}
 : ${NAMESPACE:=$XDG_RUNTIME_DIR/plan9/srv}
 
 export NAMESPACE
-mkdir -p $NAMESPACE
+mkdir -p $NAMESPACE 2>/dev/null
 exec @plan9port@/bin/9 "$@"
