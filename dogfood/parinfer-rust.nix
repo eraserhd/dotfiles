4049621268b6f3ca165fe4daf907dda20adcc459
Jason Felice <jason.m.felice@gmail.com>
2019-11-11 12:13:49 -0500

    Get this building

--- dogfood/parinfer-rust.nix
+++ dogfood/parinfer-rust.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "parinfer-rust";
-  rev = "6809233baf080e8d592cc4eb370b56a3509c235f";
-  sha256 = "0y0lhs8lplhxcv4c6h68f8j6nx44rs4jvdvy52bmxwgwariirgsy";
+  rev = "c1f74c7a0fc1329e4fde1876090554f5db227319";
+  sha256 = "12wf16gnqr66yhdbv1rcsiy71v28xrq2pk8gpbdbq09i9zsa8jhs";
 }
