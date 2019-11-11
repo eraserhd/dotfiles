190ba13cfc3f9e8141fe84f40ee00b20f0846c3d
Jason Felice <jason.m.felice@gmail.com>
2019-11-11 15:56:11 -0500

    Set up taskwarrior review

--- taskwarrior/default.nix
+++ taskwarrior/default.nix
@@ -12,6 +12,15 @@
         data.location=~/src/data/tasks
         reserved.lines=2
 
+        # tasksh Review
+        uda.reviewed.type=date
+        uda.reviewed.label=Reviewed
+        report._reviewed.description=Tasksh review report. Adjust the filter to your needs.
+        report._reviewed.columns=uuid
+        report._reviewed.sort=reviewed+,modified+
+        report._reviewed.filter=( reviewed.none: or reviewed.before:now-6days ) and ( +PENDING or +WAITING )
+
+        # Colors
         rule.precedence.color=deleted,completed,active,keyword.,tag.,project.,overdue,scheduled,due.today,due,blocked,blocking,recurring,tagged,uda.
 
         # General decoration
