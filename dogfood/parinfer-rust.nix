d2b9179b2a2d2d772a1c443bfb11cceeeb0b98f9
Jason Felice <jason.m.felice@gmail.com>
2019-11-14 19:28:51 -0500

    Bump parinfer-rust

--- dogfood/parinfer-rust.nix
+++ dogfood/parinfer-rust.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "parinfer-rust";
-  rev = "f43b9ba5eca8237682fff969471027f9e1bc3809";
-  sha256 = "1wcc56kkjnl10hp1fngwr1m4nmhsdg1dqhhqp0mmlizn3lkb575r";
+  rev = "8ae77d668e06992224768814efc1e04bb0265f94";
+  sha256 = "1lwfs7z2dsjhwpg1ygn0s34jji3pan0jqksxgkqrazv5yrkd8pim";
 }
