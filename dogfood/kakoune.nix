4bde8d519c8d3b3aa90eb71a61378bef8b873304
Jason Felice <jason.m.felice@gmail.com>
2019-11-15 00:55:10 -0500

    Bump kakoune

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "5662327c1386f8dab9c98630dd934fbdfdc79176";
-  sha256 = "0q0y22r660vrzz5mbqj920l8cm3ijhbhnrprpw2d8m8mrq7srfiv";
+  rev = "431b0893a912f84774b4c7e4d44fd606ab511067";
+  sha256 = "01g64l1lcx7rmy9547lk57zm343x23i1h0fyyfxdny20m8sdrjyi";
 }
