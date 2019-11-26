d3caba6becc0a3fd0a36f0de2d8244f702fc0ec2
Jason Felice <jason.m.felice@gmail.com>
2019-11-26 01:46:10 -0500

    Bump kak-plumb

--- dogfood/kak-plumb.nix
+++ dogfood/kak-plumb.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kak-plumb";
-  rev = "49969f16c57ec4a5be36fad286b925ee000f4f01";
-  sha256 = "0588zr8kjnglpwgipn27cyyva7dc1s71bq525dmd3ma4l7fla8g0";
+  rev = "110c0e5754bd934bfbff56600285d16a5b868ee6";
+  sha256 = "1arv5w4jmdsa1mkb9091r6cbxsfjy1k257x54g035nawj1x462dg";
 }
