c6e0befe32ce8c2d75330f2789cb33844542e252
Jason Felice <jason.m.felice@gmail.com>
2019-12-18 10:35:47 -0500

    Bump rep for ns fix

--- dogfood/rep.nix
+++ dogfood/rep.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "rep";
-  rev = "2a8638a442714b2670ce014c4cffa4273be2826f";
-  sha256 = "10nv6pad5pjpn7sm18yjlrai0g16k3v83avaf4k71p3zh8wf3r30";
+  rev = "0d4389f5b03e11d7841972e0312190f8efc0d5b1";
+  sha256 = "1m3j3d6s28dljpx9cg9is6hw9qvpd3fysrazc6zy9akf5iljp8yd";
 }
