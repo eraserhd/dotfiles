13cef9b7d93ab1ebb10f0385323f520652a06742
Jason Felice <jason.m.felice@gmail.com>
2019-12-26 00:39:14 -0500

    Do not need bleeding edge kernel

--- crunch.nix
+++ crunch.nix
@@ -16,7 +16,6 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.cleanTmpDir = true;
 
   local.systemDisplayName = "crunch";
