58b76af16f5873731f7ecd618ef133534ae948f7
Jason Felice <jason.m.felice@gmail.com>
2019-12-26 00:17:09 -0500

    Kakoune fixes

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "13d12475961b6b619566026dd334ca1a26f66071";
-  sha256 = "09zkxa1h0f2fffb4a4wkk7h63jvrzi7iq10hzcfblyhnrcafr98w";
+  rev = "fc256f8a582a5f610814de393b0372179eb152d3";
+  sha256 = "05yr45f1kpp8y54r4cd697951ds0hvdj7h55fwyjava3fvx99cws";
 }
