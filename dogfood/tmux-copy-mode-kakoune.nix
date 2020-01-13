be3e529e242d2a20a4fe329cb04cb91048502a83
Jason Felice <jason.m.felice@gmail.com>
2020-01-13 12:52:01 -0500

    Bump tmux-copy-mode-kakoune

--- dogfood/tmux-copy-mode-kakoune.nix
+++ dogfood/tmux-copy-mode-kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "tmux-copy-mode-kakoune";
-  rev = "a349932dd3978336bce00774d0ae0908c496119a";
-  sha256 = "1d0hp9b5v3gxcmsp8dch80m9jhr7bnbiwklx5gpd3r7alwmbvfga";
+  rev = "87c04cba3028fbe465e5c4fb1ea281dcaa04ca86";
+  sha256 = "0kd8dgwlxkr0192m6kaqmrhylk5ijpvn4sq6kr6rbscgzrrmpw4w";
 }
