b09212a6f0e90f628480bec1d2ccbe0990b08339
Jason Felice <jason.m.felice@gmail.com>
2020-02-03 10:14:35 -0500

    Add vcs_info to prompt

--- zsh/default.nix
+++ zsh/default.nix
@@ -14,9 +14,17 @@ in {
       eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
     '';
     programs.zsh.promptInit = ''
-      export PROMPT='
-      %(?.%F{green}.%F{red}[%?] )%n@%m %1d
+      autoload -Uz vcs_info
+      precmd() {
+        vcs_info
+      }
+      zstyle ':vcs_info:*' formats '(%b)'
+      zstyle ':vcs_info:*' actionformats '%a (%b)'
+      setopt prompt_subst
+      PROMPT='
+      %(?.%F{green}.%F{red}[%?] )%n@%m %1d ''${vcs_info_msg_0_}
       $%f '
+      RPS1=""
     '';
   };
 }
