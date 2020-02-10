271c10020cc0034b0849890e99243ed734df59e8
Jason Felice <jason.m.felice@gmail.com>
2020-02-10 11:44:24 -0500

    Remove tasksh

--- taskwarrior/default.nix
+++ taskwarrior/default.nix
@@ -3,7 +3,6 @@
 {
   config = {
     environment.systemPackages = with pkgs; [
-      tasksh
       taskwarrior
     ];
 
@@ -12,14 +11,6 @@
         data.location=~/src/data/tasks
         reserved.lines=2
         confirmation=no
-
-        # tasksh Review
-        uda.reviewed.type=date
-        uda.reviewed.label=Reviewed
-        report._reviewed.description=Tasksh review report. Adjust the filter to your needs.
-        report._reviewed.columns=uuid
-        report._reviewed.sort=reviewed+,modified+
-        report._reviewed.filter=( reviewed.none: or reviewed.before:now-6days ) and ( +PENDING or +WAITING )
 
         report.review.description=Weekly review
         report.review.columns=id,project,description.combined
