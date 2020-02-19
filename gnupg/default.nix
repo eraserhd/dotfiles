71e32c3b93714a920f8270763d87a2ae7a1c5ba5
Jason Felice <jason.m.felice@gmail.com>
2020-02-19 09:36:29 -0500

    Need pinentry

--- gnupg/default.nix
+++ gnupg/default.nix
@@ -5,5 +5,10 @@
     home-manager.users.jfelice = { pkgs, ... }: {
       home.file.".gnupg/gpg.conf".source = ./gpg.conf;
     };
+
+    environment.systemPackages = with pkgs; [
+      gnupg
+      pinentry
+    ];
   };
 }
