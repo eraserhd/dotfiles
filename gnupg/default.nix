652ad63398c63813ed1835463ec5920ab55e649d
Jason Felice <jason.m.felice@gmail.com>
2020-02-19 09:52:09 -0500

    Set up GPG pinentry

--- gnupg/default.nix
+++ gnupg/default.nix
@@ -3,12 +3,15 @@
 {
   config = {
     home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".gnupg/gpg-agent.conf".text = ''
+        pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+      '';
       home.file.".gnupg/gpg.conf".source = ./gpg.conf;
     };
 
     environment.systemPackages = with pkgs; [
       gnupg
-      pinentry
+      pinentry-curses
     ];
   };
 }
