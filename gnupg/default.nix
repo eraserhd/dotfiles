d89501826996c0c3b3c3d63cf706b898f959fa32
Jason Felice <jason.m.felice@gmail.com>
2020-02-19 23:23:16 -0500

    Do not try to install pinentry-curses on Mac

--- gnupg/default.nix
+++ gnupg/default.nix
@@ -1,17 +1,27 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+with lib;
 {
-  config = {
-    home-manager.users.jfelice = { pkgs, ... }: {
-      home.file.".gnupg/gpg-agent.conf".text = ''
-        pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
-      '';
-      home.file.".gnupg/gpg.conf".source = ./gpg.conf;
-    };
+  config = mkMerge [
+    {
+      home-manager.users.jfelice = { pkgs, ... }: {
+        home.file.".gnupg/gpg.conf".source = ./gpg.conf;
+      };
 
-    environment.systemPackages = with pkgs; [
-      gnupg
-      pinentry-curses
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        gnupg
+      ];
+    }
+    # pinentry-curses somehow requires gcr which isn't available on Mac
+    (mkIf (!pkgs.stdenv.isDarwin) {
+      home-manager.users.jfelice = { pkgs, ... }: {
+        home.file.".gnupg/gpg-agent.conf".text = ''
+          pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+        '';
+      };
+      environment.systemPackages = with pkgs; [
+        pinentry-curses
+      ];
+    })
+  ];
 }
