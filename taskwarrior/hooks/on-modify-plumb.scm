22539379fba292ad0263ae9518ef8cdab595a34c
Jason Felice <jason.m.felice@gmail.com>
2020-02-22 22:13:17 -0500

    Extract if-let

--- taskwarrior/hooks/on-modify-plumb.scm
+++ taskwarrior/hooks/on-modify-plumb.scm
@@ -1,16 +1,23 @@
 (include "json#.scm")
 
+(define-macro (if-let binding then . else)
+  (let ((tmp (gensym)))
+    `(let ((,tmp ,(cadr binding)))
+       (if ,tmp
+         (let ((,(car binding) ,tmp))
+           ,then)
+         ,@else))))
+
 (define (started? task)
   (not (eq? #f (table-ref task "start" #f))))
 
 (define (plumb-annotations task)
   (for-each
     (lambda (annotation)
-      (let ((desc (table-ref annotation "description" #f)))
-        (if desc
-          (let ((proc (open-process (list path: "9" arguments: (list "plumb" desc)))))
-            (process-status proc)
-            (close-port proc)))))
+      (if-let [desc (table-ref annotation "description" #f)]
+        (let ((proc (open-process (list path: "9" arguments: (list "plumb" desc)))))
+          (process-status proc)
+          (close-port proc))))
     (vector->list (table-ref task "annotations" #()))))
 
 (define (main . args)
@@ -23,4 +30,4 @@
       (plumb-annotations modified-task))
     0))
 
-(exit (apply main (command-line)))
+;(exit (apply main (command-line)))
