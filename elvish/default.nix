cbf168cb08436d47ad2f97af7e42919e9681d7a7
Jason Felice <jason.m.felice@gmail.com>
2020-03-09 10:50:43 -0400

    Configure elvish

--- elvish/default.nix
+++ elvish/default.nix
@@ -2,6 +2,33 @@
 
 {
   config = {
+    local.loginShell.package = pkgs.elvish;
     environment.systemPackages = [ pkgs.elvish ];
+
+    home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".elvish/rc.elv".text = ''
+        use direnv
+      '';
+      home.file.".elvish/lib/direnv.elv".text = ''
+        ## hook for direnv
+        @edit:before-readline = $@edit:before-readline {
+          try {
+            m = [("/nix/store/bp221n7s57hr1vj2hc1g12vawcx74h51-direnv-2.21.2-bin/bin/direnv" export elvish | from-json)]
+            if (> (count $m) 0) {
+              m = (explode $m)
+              keys $m | each [k]{
+                if $m[$k] {
+                  set-env $k $m[$k]
+                } else {
+                  unset-env $k
+                }
+              }
+            }
+          } except e {
+            echo $e
+          }
+        }
+      '';
+    };
   };
 }
--- zsh/default.nix
+++ zsh/default.nix
@@ -1,10 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-  shellPackage = config.local.loginShell.package;
-in {
+{
   config = {
-    local.loginShell.package = pkgs.zsh;
     programs.zsh.enable = true;
     programs.zsh.enableCompletion = true;
     programs.zsh.interactiveShellInit = ''
