9f7efe05d02df92632a4bc221d4736028d8c64cf
Jason Felice <jason.m.felice@gmail.com>
2020-03-09 11:08:46 -0400

    Fix direnv hook

--- elvish/default.nix
+++ elvish/default.nix
@@ -14,7 +14,7 @@
         ## hook for direnv
         @edit:before-readline = $@edit:before-readline {
           try {
-            m = [("/nix/store/bp221n7s57hr1vj2hc1g12vawcx74h51-direnv-2.21.2-bin/bin/direnv" export elvish | from-json)]
+            m = [("${pkgs.direnv}/bin/direnv" export elvish | from-json)]
             if (> (count $m) 0) {
               m = (explode $m)
               keys $m | each [k]{
