2e33b15f233cc121078d2333afd3fc054add6170
Jason Felice <jason.m.felice@gmail.com>
2020-03-17 09:20:19 -0400

    Use builtin-terminal-ui branch

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "329f291ae0ee7fc8841fcbd21140e1ae023c54b9";
-  sha256 = "13xvlv32ycl23ihh0jinjvfdkwynha8mdn118mrl8qf4rqsxkh5m";
+  rev = "13f33e23a1d1ad09b4cb069bec803ade671a34b4";
+  sha256 = "1v2385bcgwbb522w7ggsvimyk1m8cvrasrj2i9hlr3n93w7wb0s9";
 }
