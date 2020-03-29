6e15c09c0680367cd4b96d7a627518416c275859
Jason Felice <jason.m.felice@gmail.com>
2020-03-29 15:08:16 -0400

    Do not use broken builtin-terminal-ui

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "47b7d154302b11c7215b61bb00c0d45167107795";
-  sha256 = "16llhznq1rgjv7yvl9pb6xky42kvd61h5zzvgij1yxzkwj951vqs";
+  rev = "b42885ef71e73a5af4f45c69aaeb63e6890dfa3b";
+  sha256 = "0hkm1y6i87acqfjcqqd6zpg80748m1c74passk4x3s39k4v22j2f";
 }
