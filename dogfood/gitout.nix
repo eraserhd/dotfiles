2eb058ea9d8aa8ff36c1beae4344288a61e7deea
Jason Felice <jason.m.felice@gmail.com>
2020-03-31 15:56:23 -0400

    Use new github-search

--- dogfood/gitout.nix
+++ dogfood/gitout.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "gitout";
-  rev = "1717aafe80da816675790ae31c6417ca9188dc33";
-  sha256 = "1kkx8x4b9nii5cxh7nb9bqjg7g68389mc2wihpffj66i708hbbfg";
+  rev = "823107ff6c14af6f3f4ceb10490ce72ef95e0322";
+  sha256 = "1psjqav95wv5wckfwj8vllyqifppian0lvbdadwskcp1qn90f45r";
 }
