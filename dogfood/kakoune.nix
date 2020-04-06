49db424f88f3f030e4a6009a7c7a1fe2065bb591
Jason Felice <jason.m.felice@gmail.com>
2020-04-06 09:18:11 -0400

    Weird, ok

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "e9709af1684e96d2351b3d56b48d46048f4a90e8";
+  rev = "a714c11c4500a50ca440ee832cfaee6ee7d90907";
   sha256 = "08ydkl25n33wyp4vknzxq2xllncdass63wdlw7qcbl2wnzjm0r8g";
 }
