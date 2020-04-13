e84ce21e7f38ec2bfcb77d2499699f60585f4f3a
Jason Felice <jason.m.felice@gmail.com>
2020-04-13 10:00:31 -0400

    Update yabai config

--- skhd/default.nix
+++ skhd/default.nix
@@ -14,6 +14,22 @@
       ctrlw < j : yabai -m window --focus south ; skhd -k escape
       ctrlw < k : yabai -m window --focus north ; skhd -k escape
       ctrlw < l : yabai -m window --focus east ; skhd -k escape
+
+      ctrlw < r : yabai -m window --focus last ; skhd -k escape
+
+      ctrlw < 0x1D : yabai -m window --focus first ; skhd -k escape
+      ctrlw < 0x1E : \
+        yabai -m window --focus first ; \
+        for (( i = 0; i < 1; i++ )); do \
+          yabai -m window --focus next ; \
+        done ; \
+        skhd -k escape
+      ctrlw < 0x1F : \
+        skhd -k escape ; \
+        yabai -m window --focus first ; \
+        for (( i = 0; i < 2; i++ )); do \
+          yabai -m window --focus next ; \
+        done
     '';
     environment.systemPackages = [ pkgs.skhd ];
   }
