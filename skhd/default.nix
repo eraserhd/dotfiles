69045db078d125761e443b67be200ebc11ec78eb
Jason Felice <jason.m.felice@gmail.com>
2020-04-12 21:18:22 -0400

    Make C-w h/j/k/l work better

--- skhd/default.nix
+++ skhd/default.nix
@@ -1,11 +1,21 @@
-{ options, ... }:
+{ options, pkgs, ... }:
 
 {
   config = if (builtins.hasAttr "skhd" options.services)
   then {
     services.skhd.enable = true;
     services.skhd.skhdConfig = ''
+
+      :: ctrlw @
+
+      ctrl - w ; ctrlw
+      ctrlw < escape ; default
+      ctrlw < h : yabai -m window --focus west ; skhd -k escape
+      ctrlw < j : yabai -m window --focus south ; skhd -k escape
+      ctrlw < k : yabai -m window --focus north ; skhd -k escape
+      ctrlw < l : yabai -m window --focus east ; skhd -k escape
     '';
+    environment.systemPackages = [ pkgs.skhd ];
   }
   else {
   };
