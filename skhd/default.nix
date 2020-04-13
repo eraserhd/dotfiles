9436c51c782f9a2e95ff6c97771d8641b0499384
Jason Felice <jason.m.felice@gmail.com>
2020-04-13 14:52:56 -0400

    C-w s mode

--- skhd/default.nix
+++ skhd/default.nix
@@ -5,11 +5,12 @@
   then {
     services.skhd.enable = true;
     services.skhd.skhdConfig = ''
-
       :: ctrlw @
+      :: swap @
 
       ctrl - w ; ctrlw
       ctrlw < escape ; default
+
       ctrlw < h : yabai -m window --focus west ; skhd -k escape
       ctrlw < j : yabai -m window --focus south ; skhd -k escape
       ctrlw < k : yabai -m window --focus north ; skhd -k escape
@@ -18,18 +19,16 @@
       ctrlw < r : yabai -m window --focus last ; skhd -k escape
 
       ctrlw < 0x1D : yabai -m window --focus first ; skhd -k escape
-      ctrlw < 0x1E : \
-        yabai -m window --focus first ; \
-        for (( i = 0; i < 1; i++ )); do \
-          yabai -m window --focus next ; \
-        done ; \
-        skhd -k escape
-      ctrlw < 0x1F : \
-        skhd -k escape ; \
-        yabai -m window --focus first ; \
-        for (( i = 0; i < 2; i++ )); do \
-          yabai -m window --focus next ; \
-        done
+
+      ctrlw < s ; swap
+      swap < escape ; default
+
+      swap < h : yabai -m window --swap west ; skhd -k escape
+      swap < j : yabai -m window --swap south ; skhd -k escape
+      swap < k : yabai -m window --swap north ; skhd -k escape
+      swap < l : yabai -m window --swap east ; skhd -k escape
+
+      swap < r : yabai -m window --swap last ; skhd -k escape
     '';
     environment.systemPackages = [ pkgs.skhd ];
   }
