2aa97a38bd5d283db2c7b733098af7f582e110f8
Jason Felice <jason.m.felice@gmail.com>
2020-05-04 11:13:29 -0400

    Revert "Start using gewgaw"
    This reverts commit 791a0c90f99ad0ac77b37827e275ce90e8a6b3fc.

--- dogfood/default.nix
+++ dogfood/default.nix
@@ -21,8 +21,6 @@
           src = super.pkgs.fetchFromGitHub (import ./gerbil.nix);
         });
 
-        gewgaw = super.callPackage "${super.fetchFromGitHub (import ./gewgaw.nix)}/derivation.nix" {};
-
         inherit gerbilPackages;
 
         gitAndTools = super.gitAndTools // {
