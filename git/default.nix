9bb5c581a81283b3de3de37bf24f7641ed04ce55
Jason Felice <jason.m.felice@gmail.com>
2020-05-05 09:35:03 -0400

    Use default location for global gitignore

--- git/default.nix
+++ git/default.nix
@@ -21,7 +21,13 @@
     home-manager.users.jfelice = { pkgs, ... }: {
       home.file.".config/hub".source = ./hub;
       home.file.".gitconfig".source = ./gitconfig;
-      home.file.".gitignore_global".source = ./gitignore_global;
+      home.file.".config/git/ignore".text = ''
+        .DS_Store
+        .*.swp
+        tags
+        build-deps
+        TAGS
+      '';
     };
   };
 }
