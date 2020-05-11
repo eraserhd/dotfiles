3f13b4ad7ca651e7861f1a476085bbe58334700a
Jason Felice <jason.m.felice@gmail.com>
2020-05-11 10:38:51 -0400

    Edit command line in editor

--- zsh/default.nix
+++ zsh/default.nix
@@ -6,6 +6,11 @@
     programs.zsh.enable = true;
     programs.zsh.enableCompletion = true;
     programs.zsh.interactiveShellInit = ''
+      autoload -U edit-command-line
+      zle -N edit-command-line
+      bindkey '^xe' edit-command-line
+      bindkey '^x^e' edit-command-line
+
       setopt auto_cd
       cdpath=(~/src)
 
