e0a5d45d23b7bc532d4ec9e0e4936dbe04ccd85a
Jason Felice <jason.m.felice@gmail.com>
2020-05-12 10:43:36 -0400

    Ensure login shell, extract yabai-config

--- yabai/default.nix
+++ yabai/default.nix
@@ -3,7 +3,12 @@
 {
   config = {
     home-manager.users.jfelice = { pkgs, ... }: {
-      home.file.".yabairc".source = ./yabairc;
+      home.file.".yabairc" = {
+        executable = true;
+        text = ''
+          exec ${pkgs.zsh}/bin/zsh -l -c 'yabai-config'
+        '';
+      };
     };
   };
 }
