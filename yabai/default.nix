08d9a48cf1a534e88fcc1b6513f9dc76fe7fdd8d
Jason Felice <jason.m.felice@gmail.com>
2020-05-23 22:57:25 -0400

    Make yabai config parse on NixOS

--- yabai/default.nix
+++ yabai/default.nix
@@ -1,12 +1,14 @@
-{ lib, pkgs, ... }:
+{ options, pkgs, ... }:
 
-with lib;
 {
-  config = mkIf pkgs.stdenv.isDarwin {
+  config = (if (builtins.hasAttr "yabai" options.services)
+  then {
     services.yabai = {
       enable = true;
       package = pkgs.yabai;
       extraConfig = "exec ${pkgs.zsh}/bin/zsh -l -c 'yabai-config'";
     };
-  };
+  }
+  else {
+  });
 }
