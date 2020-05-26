763dbc879ee3bac71c90f864b8ba9cabbac44721
Jason Felice <jason.m.felice@gmail.com>
2020-05-26 11:54:23 -0400

    Start plumber automatically

--- networking/plan9/plumber/default.nix
+++ networking/plan9/plumber/default.nix
@@ -1,9 +1,25 @@
-{ config, lib, ... }:
+{ config, lib, options, pkgs, ... }:
 
 with lib;
 let
   cfg = config.local.plan9.terminal;
 in {
-  config = mkIf cfg.enable {
-  };
+  config = mkIf cfg.enable (if (builtins.hasAttr "launchd" options)
+  then {
+    launchd.user.agents.plumber = {
+      script = ''
+        exec zsh -l -c '
+          ${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace \
+              ${pkgs.plan9port-wrapper}/bin/9 plumber -p ${./plumbing} -f
+        '
+      '';
+      serviceConfig = {
+        KeepAlive = true;
+        StandardOutPath = "/tmp/out2.log";
+        StandardErrorPath = "/tmp/err2.log";
+      };
+    };
+  }
+  else {
+  });
 }
