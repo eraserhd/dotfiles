e61953d9de698baef431a0a1d04360b0bdf471d2
Jason Felice <jason.m.felice@gmail.com>
2020-05-30 01:22:49 -0400

    Bump kak-ansi for scrollback

--- dogfood/kak-ansi.nix
+++ dogfood/kak-ansi.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kak-ansi";
-  rev = "10843e04b0cb449ee928061743f714550966ee28";
-  sha256 = "19iqs99a3h1fpx65fwv45h5jybvbpbch5pzv5l7ml4inrpxqxx2p";
+  rev = "4d9ca803d6c60d1fb2d46838dae9874fe5e52dab";
+  sha256 = "037392z2arxir0gaassq34017b7rdnrfswf6ldr95hc3k8my28sp";
 }
