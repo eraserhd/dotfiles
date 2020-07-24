6934bcf13555b60b7d22d51b3f91905a80d4f348
Jason Felice <jason.m.felice@gmail.com>
2020-07-24 17:07:08 -0400

    Install Homebrew stuff during activation

--- modules/nix-darwin/homebrew/default.nix
+++ modules/nix-darwin/homebrew/default.nix
@@ -1,9 +1,27 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 with lib;
 let
   cfg = config.homebrew;
 
+  caskAppdirStatement = if (isNull cfg.cask_args.appdir)
+                        then ""
+                        else "cask_args: appdir: '${cfg.cask_args.appdir}'";
+
+  statements = word: values: (concatMapStrings (value: "${word} '${value}'\n") values);
+
+  brewfile = pkgs.writeTextFile {
+    name = "Brewfile";
+    text = ''
+      ${caskAppdirStatement}
+
+      ${statements "tap" cfg.taps}
+
+      ${statements "cask" cfg.casks}
+
+      ${statements "brew" cfg.formulae}
+    '';
+  };
 
 in {
   options.homebrew = {
@@ -12,28 +30,36 @@ in {
     taps = mkOption {
       description = "taps to retrieve formulae from";
       type = with types; listOf str;
+      example = "homebrew/cask-fonts";
     };
 
     cask_args.appdir = mkOption {
       description = "path for Mac OS application bundles";
       type = with types; nullOr path;
       default = null;
-      example = ''???
-      '';
+      example = "/Applications";
     };
 
     casks = mkOption {
       description = "casks to be installed";
       type = with types; listOf str;
+      default = [];
+      example = "spotify";
     };
 
     formulae = mkOption {
       description = "formulae to be installed";
       type = with types; listOf str;
+      default = [];
+      example = "openscad";
     };
   };
 
   config = mkIf cfg.enable {
     homebrew.taps = [ "homebrew/bundle" ];
+
+    system.activationScripts.extraUserActivation.text = ''
+      HOMEBREW_NO_AUTO_UPDATE=1 /usr/local/bin/brew bundle --file="${brewfile}"
+    '';
   };
 }
