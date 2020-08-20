024ff7d8fcf08b387de4fccd26d675c0aea3a6ef
Jason Felice <jason.m.felice@gmail.com>
2020-08-20 11:07:52 -0400

    Turn off antialiasing on crunch

--- machines/crunch/default.nix
+++ machines/crunch/default.nix
@@ -73,6 +73,7 @@
   services.xserver.displayManager.sessionCommands = ''
     ${pkgs.xlibs.xset}/bin/xset r rate 200 60
   '';
+  fonts.fontconfig.antialias = false;
 
   nix.nixPath = [
     "nixpkgs=/home/jfelice/src/dotfiles/nixpkgs"
