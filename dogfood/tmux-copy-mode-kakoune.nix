634fae22a98769a0760079428f6d322f25d6908f
Jason Felice <jason.m.felice@gmail.com>
2020-09-14 12:11:29 -0400

    Remove unused dogfood files

--- dogfood/tmux-copy-mode-kakoune.nix
+++
@@ -1,6 +0,0 @@
-{
-  owner = "eraserhd";
-  repo = "tmux-copy-mode-kakoune";
-  rev = "87c04cba3028fbe465e5c4fb1ea281dcaa04ca86";
-  sha256 = "0kd8dgwlxkr0192m6kaqmrhylk5ijpvn4sq6kr6rbscgzrrmpw4w";
-}
--- dogfood/tmux.nix
+++
@@ -1,6 +0,0 @@
-{
-  owner = "eraserhd";
-  repo = "tmux";
-  rev = "bed3c0ac1ee8bb30332ee32e20abf64924d3dfac";
-  sha256 = "1mzfbi9bp01yg7fds0rdja49zsqsi0kk9ka89zlh3xxgf1zdlmch";
-}
