9561d0f2a077364a3f254668bde21ca1598df81b
Jason Felice <jason.m.felice@gmail.com>
2020-09-29 11:56:51 -0400

    Move into CtrlW:init()

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,7 +26,6 @@ function Mode:append(keys)
 end
 
 local CtrlW = {}
-local ctrlw
 
 function CtrlW:new()
   newObj = {
@@ -45,6 +44,61 @@ function CtrlW:new()
 end
 
 function CtrlW:init()
+  local function window_number(n)
+    local windows = hs.window.visibleWindows()
+    local bounds = hs.screen.mainScreen():frame()
+    local screen_windows = {}
+    for _, w in ipairs(windows) do
+      local wframe = w:frame()
+      if w:frame():inside(bounds) then
+        table.insert(screen_windows, w)
+      end
+    end
+    windows = screen_windows
+    table.sort(windows, function(a, b)
+      local af, bf = a:frame(), b:frame()
+      if af.x < bf.x then return true end
+      if af.x > bf.x then return false end
+      return af.y < bf.y
+    end)
+    if n == 9 or n > #windows then
+      return windows[#windows]
+    else
+      return windows[n+1]
+    end
+  end
+
+  local function hotkey_exec(mode_table, mods, key, command)
+    mode_table:append({
+      hs.hotkey.new(mods, key, function()
+        self:enter_mode("default")
+        hs.execute(command, true)
+      end)
+    })
+  end
+
+  hotkey_exec(self.modes.ctrlw, {},        "H", "yabai -m window --focus west")
+  hotkey_exec(self.modes.ctrlw, {},        "J", "yabai -m window --focus south")
+  hotkey_exec(self.modes.ctrlw, {},        "K", "yabai -m window --focus north")
+  hotkey_exec(self.modes.ctrlw, {},        "L", "yabai -m window --focus east")
+  hotkey_exec(self.modes.ctrlw, {},        "P", "yabai -m window --focus recent")
+  hotkey_exec(self.modes.ctrlw, {},        "R", "kitty @ --to unix:/Users/jfelice/.run/kitty focus-window --match=title:kak_repl_window")
+  hotkey_exec(self.modes.ctrlw, {"shift"}, "R", "kitty @ --to unix:/Users/jfelice/.run/kitty focus-window --match=title:shell_window")
+
+  for i=0,9 do
+    self.modes.ctrlw:append({
+      hs.hotkey.new({}, tostring(i), function()
+        self:enter_mode("default")
+        local window = window_number(i)
+        window:focus()
+      end)
+    })
+  end
+
+  hotkey_exec(self.modes.ctrlw, {}, "C", "yabai-focus-space code")
+  hotkey_exec(self.modes.ctrlw, {}, "F", "yabai-focus-space focus")
+  hotkey_exec(self.modes.ctrlw, {}, "B", "yabai-focus-space browse")
+
   self:enter_mode("default")
 end
 
@@ -57,60 +111,4 @@ function CtrlW:enter_mode(mode_name)
 end
 
 ctrlw = CtrlW:new()
-
-function window_number(n)
-  local windows = hs.window.visibleWindows()
-  local bounds = hs.screen.mainScreen():frame()
-  local screen_windows = {}
-  for _, w in ipairs(windows) do
-    local wframe = w:frame()
-    if w:frame():inside(bounds) then
-      table.insert(screen_windows, w)
-    end
-  end
-  windows = screen_windows
-  table.sort(windows, function(a, b)
-    local af, bf = a:frame(), b:frame()
-    if af.x < bf.x then return true end
-    if af.x > bf.x then return false end
-    return af.y < bf.y
-  end)
-  if n == 9 or n > #windows then
-    return windows[#windows]
-  else
-    return windows[n+1]
-  end
-end
-
-function hotkey_exec(mode_table, mods, key, command)
-  mode_table:append({
-    hs.hotkey.new(mods, key, function()
-      ctrlw:enter_mode("default")
-      hs.execute(command, true)
-    end)
-  })
-end
-
-hotkey_exec(ctrlw.modes.ctrlw, {},        "H", "yabai -m window --focus west")
-hotkey_exec(ctrlw.modes.ctrlw, {},        "J", "yabai -m window --focus south")
-hotkey_exec(ctrlw.modes.ctrlw, {},        "K", "yabai -m window --focus north")
-hotkey_exec(ctrlw.modes.ctrlw, {},        "L", "yabai -m window --focus east")
-hotkey_exec(ctrlw.modes.ctrlw, {},        "P", "yabai -m window --focus recent")
-hotkey_exec(ctrlw.modes.ctrlw, {},        "R", "kitty @ --to unix:/Users/jfelice/.run/kitty focus-window --match=title:kak_repl_window")
-hotkey_exec(ctrlw.modes.ctrlw, {"shift"}, "R", "kitty @ --to unix:/Users/jfelice/.run/kitty focus-window --match=title:shell_window")
-
-for i=0,9 do
-  ctrlw.modes.ctrlw:append({
-    hs.hotkey.new({}, tostring(i), function()
-      ctrlw:enter_mode("default")
-      local window = window_number(i)
-      window:focus()
-    end)
-  })
-end
-
-hotkey_exec(ctrlw.modes.ctrlw, {}, "C", "yabai-focus-space code")
-hotkey_exec(ctrlw.modes.ctrlw, {}, "F", "yabai-focus-space focus")
-hotkey_exec(ctrlw.modes.ctrlw, {}, "B", "yabai-focus-space browse")
-
 ctrlw:init()
