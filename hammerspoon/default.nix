7d77c81a50366d8aa2e2163ccd5b0a811f050838
Jason Felice <jason.m.felice@gmail.com>
2020-09-29 13:17:58 -0400

    Check syntax during build

--- hammerspoon/default.nix
+++ hammerspoon/default.nix
@@ -1,11 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
-{
+let
+  checkedConfig = pkgs.stdenv.mkDerivation {
+    name = "hammerspoon-config";
+    src = ./.;
+    buildInputs = with pkgs; [ lua ];
+    buildPhase = ''
+      luac -p init.lua
+    '';
+    installPhase = ''
+      mkdir $out
+      cp init.lua $out/
+    '';
+  };
+in {
   homebrew.casks = [
     "hammerspoon"
   ];
 
   home-manager.users.jfelice = { pkgs, ... }: {
-    home.file.".hammerspoon/init.lua".source = ./init.lua;
+    home.file.".hammerspoon/init.lua".source = "${checkedConfig}/init.lua";
   };
 }
