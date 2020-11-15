9439a1588e55b237bfd5e8ac6a42cb98cd0d48bd
Jason Felice <jason.m.felice@gmail.com>
2020-11-15 16:32:51 -0500

    Install openscad-cli

--- 3d/default.nix
+++ 3d/default.nix
@@ -1,12 +1,31 @@
-{ options, ... }:
+{ pkgs, options, ... }:
 
-{
+let
+  openscad-cli = pkgs.stdenv.mkDerivation {
+    pname = "openscad-cli";
+    version = "0.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "onealexharms";
+      repo = "openscad-cli";
+      rev = "d4a02e92dea6a0f08b7a67313b13b043d0a9f4ec";
+      sha256 = "1l0drqpjy2p0g6i9mcrqbcap64wnq1yymrwic1m7i6cgfnrdcyn2";
+    };
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp openscad-cli $out/bin/
+      ln -s $out/bin/openscad-cli $out/bin/osc
+    '';
+  };
+in {
   config = (if (builtins.hasAttr "homebrew" options)
   then {
     homebrew.casks = [
       "openscad"
       "ultimaker-cura"
     ];
+    environment.systemPackages = [ openscad-cli ];
   }
   else {
   });
