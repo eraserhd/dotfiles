0743784fe39560adb0646717669791e3655cf8f1
Jason Felice <jason.m.felice@gmail.com>
2020-11-16 16:31:37 -0500

    Switch back because merge

--- kakoune/quickscope.kak.nix
+++ kakoune/quickscope.kak.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation rec {
   version = "unstable-2020-11-16";
 
   src = fetchgit {
-    url = "https://git.sr.ht/~eraserhd/quickscope.kak";
-    rev = "28898f971d7534123305425bb1133d6560485f70";
+    url = "https://git.sr.ht/~voroskoi/quickscope.kak";
+    rev = "ee4784147a0389766b87e07f58443a1bdf76e713";
     sha256 = "0y1g3zpa2ql8l9rl5i2w84bka8a09kig9nq9zdchaff5pw660mcx";
   };
 
@@ -16,6 +16,6 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/kak/autoload/plugins/
     cp quickscope.* $out/share/kak/autoload/plugins/
     # substituteInPlace does not like the pipe
-    sed -e 's,[|] *lua,|${lua5_3}/bin/lua,' -e 's,2>&1,,' quickscope.kak >$out/share/kak/autoload/plugins/quickscope.kak
+    sed -e 's,[|] *lua,|${lua5_3}/bin/lua,' quickscope.kak >$out/share/kak/autoload/plugins/quickscope.kak
   '';
 }
