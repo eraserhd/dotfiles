9b8634504a139ac8b52a1335d48b56b4b36dbfab
Jason Felice <jason.m.felice@gmail.com>
2020-11-20 15:24:24 -0500

    Remove traces of clj-kondo :(

--- clojure/default.nix
+++ clojure/default.nix
@@ -2,35 +2,28 @@
 
 with lib;
 {
-  config = mkMerge [
-    {
-      nixpkgs.overlays = [
-        (self: super: {
-          jdk = super.jdk11;
+  config = {
+    nixpkgs.overlays = [
+      (self: super: {
+        jdk = super.jdk11;
 
-          # fop doesn't like my jdk11 override
-          fop = (super.fop.override {
-            jdk = self.jdk8;
-          });
-        })
-      ];
+        # fop doesn't like my jdk11 override
+        fop = (super.fop.override {
+          jdk = self.jdk8;
+        });
+      })
+    ];
 
-      environment.systemPackages = with pkgs; [
-        clojure
-        leiningen
-        parinfer-rust
-        rep
-      ];
+    environment.systemPackages = with pkgs; [
+      clojure
+      leiningen
+      parinfer-rust
+      rep
+    ];
 
-      home-manager.users.jfelice = { pkgs, ... }: {
-        home.file.".clojure/deps.edn".source = ./deps.edn;
-        home.file.".lein/profiles.clj".source = ./profiles.clj;
-      };
-    }
-    #(mkIf (!pkgs.stdenv.isDarwin) {
-    #  environment.systemPackages = with pkgs; [
-    #    clj-kondo
-    #  ];
-    #})
-  ];
+    home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".clojure/deps.edn".source = ./deps.edn;
+      home.file.".lein/profiles.clj".source = ./profiles.clj;
+    };
+  };
 }
