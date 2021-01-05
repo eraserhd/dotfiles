bd73616aa63144bde370725ff7bc68e94e31bc59
Jason Felice <jason.m.felice@gmail.com>
2021-01-05 10:05:16 -0500

    Fix outputHash

--- fonts/default.nix
+++ fonts/default.nix
@@ -6,6 +6,7 @@
     fonts.fonts = with pkgs; [
       (input-fonts.overrideAttrs (oldAttrs: {
         src = ./Input-Font.zip;
+        outputHash = "14bfyjnhsbxv9v76ngn0v7fb6f7xahnmny38251nm9dhai66zqx8";
       }))
       mononoki
     ];
