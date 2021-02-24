d3b5ab3a1aa044a572c71b2f8f5b7cf0d5391792
Jason Felice <jason.m.felice@gmail.com>
2021-02-24 15:36:45 -0500

    Remove autossh config

--- machines/macbook/default.nix
+++ machines/macbook/default.nix
@@ -32,24 +32,6 @@
 
   services.nix-daemon.enable = true;
 
-  services.autossh.sessions = [ {
-    name = "crunch";
-    user = "jfelice";
-    extraArguments = " -i ${toString ../../networking/ssh/files/id_rsa-macbook}" +
-                     " -o 'StreamLocalBindUnlink yes'" +
-                     " -o 'ExitOnForwardFailure yes'" +
-                     " -o 'ServerAliveCountMax 3'" +
-                     " -o 'ServerAliveInterval 10'" +
-                     " -L8820:localhost:8820" +
-                     " -L8080:localhost:8080" +
-                     " -L3449:localhost:3449" +
-                     " -R14242:localhost:14242" + # CP dev repl
-                     " -R24242:localhost:24242" + # CP prod repl
-                     " -R/run/user/1000/plan9/srv/snarf:/Users/jfelice/.run/plan9/srv/snarf" +
-                     " -R/run/user/1000/plan9/srv/plumb:/Users/jfelice/.run/plan9/srv/plumb" +
-                     " -T -N jfelice@crunch.eraserhead.net";
-  } ];
-
   environment.xdgRuntimeDir.enable = true;
 
   users.users.jfelice = {
