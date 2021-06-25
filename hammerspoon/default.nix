c14f9b45aa40746025848685e49c5ae4d6908e1b
Jason Felice <jason.m.felice@gmail.com>
2021-06-25 16:20:43 -0400

    Install WindowSigils.spoon from Spoons repo

--- hammerspoon/default.nix
+++ hammerspoon/default.nix
@@ -13,13 +13,21 @@ let
       cp init.lua $out/
     '';
   };
+
+  WindowSigils-src = pkgs.fetchFromGitHub {
+    owner = "eraserhd";
+    repo = "Spoons";
+    rev = "91759d8a0c5214649a5af565bbb28ac950d04028";
+    sha256 = "OVDEGdUAuQqdVN0fcfJsKVoTPKUZlvZO6puGnEiN1eI=";
+  };
+
 in {
   config = {
     services.hammerspoon.enable = true;
 
     home-manager.users.jfelice = { pkgs, ... }: {
       home.file.".hammerspoon/init.lua".source = "${checkedConfig}/init.lua";
-      home.file.".hammerspoon/Spoons/WindowSigils.spoon".source = ./WindowSigils.spoon;
+      home.file.".hammerspoon/Spoons/WindowSigils.spoon".source = "${WindowSigils-src}/Source/WindowSigils.spoon";
       home.file.".hammerspoon/Spoons/Bubbles.spoon".source = ./Bubbles.spoon;
       home.file.".hammerspoon/Spoons/MouseFollowsFocus.spoon".source = ./MouseFollowsFocus.spoon;
     };
