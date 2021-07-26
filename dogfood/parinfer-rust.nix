0ed9eb8fbf25be2a3ea8b4b5d4ded57cff2f5685
Jason Felice <jason.m.felice@gmail.com>
2021-07-26 09:32:20 -0400

    Bump to fix sha

--- dogfood/parinfer-rust.nix
+++ dogfood/parinfer-rust.nix
@@ -1,7 +1,7 @@
 {
   owner = "eraserhd";
   repo = "parinfer-rust";
-  rev = "afcacc8ef1fbf23d804d2c4eb01a419c50565e44";
-  sha256 = "IHUJfwoAOaREGkD8/kJMuXwqmddRL76Ar9SHLfmDivc=";
+  rev = "1d5cc1b67ea01551b5f1151432de4af598f37dab";
+  sha256 = "/RgE13qwx6I6R4h+uYwBqMdu0gvfShfiS4m0TW2ktEU=";
   fetchSubmodules = true;
 }
