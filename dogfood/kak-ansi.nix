67c2013612f1a64913dbd695605567bfdd38adbf
Jason Felice <jason.m.felice@gmail.com>
2021-08-01 11:05:29 -0400

    Bump kak-ansi

--- dogfood/kak-ansi.nix
+++ dogfood/kak-ansi.nix
@@ -1,7 +1,7 @@
 {
   owner = "eraserhd";
   repo = "kak-ansi";
-  rev = "71e43f25be0c5fd503054f82d16c16377ce1f5f9";
-  sha256 = "F1j5IaEgFtXnJQujnZggg2pPt4m5pso1ik2r4SIQneI=";
+  rev = "240a1489ff2b026e41b0c85aea459808aa2051dc";
+  sha256 = "mf8+gP/w9oc2ufU48dJLLLOsscEd9AwtzBKxzoZt3P4=";
   fetchSubmodules = true;
 }
