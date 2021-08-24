7cda1b4137e54337e93eda78a54a5d61e6d37ee7
Jason Felice <jason.m.felice@gmail.com>
2021-08-24 16:47:23 -0400

    Remove old config

--- macos/install.sh
+++ macos/install.sh
@@ -7,9 +7,3 @@ set -e
 # Why would I ever want a single-button mouse?
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
-
-# No Command+Space for Spotlight
-# This doesn't seem to take effect immediately unless changed through the UI :(
-defaults export com.apple.symbolichotkeys /tmp/defaults.plist
-plutil -replace AppleSymbolicHotKeys.64.enabled -integer 0 /tmp/defaults.plist
-defaults import com.apple.symbolichotkeys /tmp/defaults.plist
