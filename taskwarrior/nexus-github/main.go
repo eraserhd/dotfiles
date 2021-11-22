4d0076c1cc9eb9f919c10f7bdf398df331b351f9
Jason Felice <jason.m.felice@gmail.com>
2021-11-22 00:34:10 -0500

    Get the first thing

--- taskwarrior/nexus-github/main.go
+++ taskwarrior/nexus-github/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	httpClient := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	))
+
+	client := githubv4.NewClient(httpClient)
+
+	var query struct {
+		Organization struct {
+			Repository struct {
+				Name string
+			} `graphql:"repository(name: \"nexus\")"`
+		} `graphql:"organization(login: \"coding-boot-camp\")"`
+	}
+	err := client.Query(context.Background(), &query, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Got: %s\n", query.Organization.Repository.Name)
 }
