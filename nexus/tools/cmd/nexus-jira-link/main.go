8823e756ee105aa24b631ffee729184088f97cb4
Jason Felice <jason.m.felice@gmail.com>
2021-11-24 14:23:58 -0500

    Move function into another file

--- nexus/tools/cmd/nexus-jira-link/main.go
+++ nexus/tools/cmd/nexus-jira-link/main.go
@@ -6,46 +6,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"unicode"
 
 	"github.com/eraserhd/dotfiles/nexus/tools/pkg/jira"
 )
-
-func isAllDigits(s string) bool {
-	if len(s) == 0 {
-		return false
-	}
-	for _, r := range s {
-		if !unicode.IsDigit(r) {
-			return false
-		}
-	}
-	return true
-}
-
-func JiraTickets(branch string) ([]string, error) {
-	parts := strings.Split(strings.Trim(strings.ToUpper(branch), " \t\r\n\v"), "-")
-	if len(parts) < 2 {
-		return nil, fmt.Errorf("%q does not have enough parts to be a branch name", branch)
-	}
-	var firstNumericPart int
-	for i, part := range parts {
-		if isAllDigits(part) {
-			firstNumericPart = i
-			break
-		}
-	}
-	if firstNumericPart == 0 {
-		return nil, fmt.Errorf("%q does not have a project name part", branch)
-	}
-	project := parts[firstNumericPart-1]
-	var tickets []string
-	for i := firstNumericPart; i < len(parts); i++ {
-		tickets = append(tickets, fmt.Sprintf("%s-%s", project, parts[i]))
-	}
-	return tickets, nil
-}
 
 func branchArgs() []string {
 	branches := flag.Args()
@@ -63,7 +26,7 @@ func branchArgs() []string {
 func ticketArgs() []string {
 	var tickets []string
 	for _, branch := range branchArgs() {
-		branchTickets, err := JiraTickets(branch)
+		branchTickets, err := jira.TicketsForBranchName(branch)
 		if err != nil {
 			log.Printf("parsing %q: %v", branch, err)
 			continue
