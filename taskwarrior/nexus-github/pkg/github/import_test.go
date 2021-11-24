6b29d6c550af33eb47a5f3cdb1fcac220455f08f
Jason Felice <jason.m.felice@gmail.com>
2021-11-23 19:43:26 -0500

    Uuids are repeatable

--- taskwarrior/nexus-github/pkg/github/import_test.go
+++ taskwarrior/nexus-github/pkg/github/import_test.go
@@ -7,22 +7,47 @@ import (
 	"github.com/google/uuid"
 )
 
-func Test_Uuid_is_repeatably_computed_from_PullRequest_Id(t *testing.T) {
-	pulls := OpenPullRequestsQuery{}
-	err := json.Unmarshal([]byte(`{
-		"Organization": {
-        		"Repository": {
-                		"PullRequests": {
-                        		"Edges": [ {
-                                		"Id": ""
-                        		} ]
-                		}
-        		}
-		}
-        }`), &pulls)
+func queryResults(t *testing.T, text string) OpenPullRequestsQuery {
+	var pulls OpenPullRequestsQuery
+	err := json.Unmarshal([]byte(text), &pulls)
 	if err != nil {
-		t.Errorf("wanted err == nil, got %v", err)
+		t.Errorf("unmarshal: %v", err)
 	}
+	return pulls
+}
+
+const (
+	singlePullWithId1 = `{
+		"Organization": {
+			"Repository": {
+				"PullRequests": {
+					"Edges": [ {
+						"Node": {
+							"Id": "MDExOlB1bGxSZXF1ZXN0MjEwNzk3NTAx"
+						}
+					} ]
+				}
+			}
+		}
+        }`
+
+	singlePullWithId2 = `{
+		"Organization": {
+			"Repository": {
+				"PullRequests": {
+					"Edges": [ {
+						"Node": {
+							"Id": "MDExOlB1bGxSZXF1ZXN0MjE3MDE1MDk5"
+						}
+					} ]
+				}
+			}
+		}
+        }`
+)
+
+func Test_Uuid_is_repeatably_computed_from_PullRequest_Id(t *testing.T) {
+	pulls := queryResults(t, singlePullWithId1)
 	tasks, err := pulls.Tasks()
 	if err != nil {
 		t.Errorf("wanted err == nil, got %v", err)
@@ -33,5 +58,20 @@ func Test_Uuid_is_repeatably_computed_from_PullRequest_Id(t *testing.T) {
 	var zeroUuid uuid.UUID
 	if tasks[0].Uuid == zeroUuid {
 		t.Errorf("wanted non-zero UUID, but got a zero UUID")
+	}
+	tasks2, err := pulls.Tasks()
+	if err != nil {
+		t.Errorf("wanted err == nil, got %v", err)
+	}
+	if tasks[0].Uuid != tasks2[0].Uuid {
+		t.Error("wanted uuid to be repeatable, but it was not")
+	}
+	pulls3 := queryResults(t, singlePullWithId2)
+	tasks3, err := pulls3.Tasks()
+	if err != nil {
+		t.Errorf("wanted err == nil, got %v", err)
+	}
+	if tasks[0].Uuid == tasks3[0].Uuid {
+		t.Errorf("wanted tasks[0].Uuid != tasks3.Uuid, both are %v", tasks3[0].Uuid)
 	}
 }
