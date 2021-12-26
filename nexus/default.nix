1fa04f643a932f044353dc2842d27659f2153566
Jason Felice <jason.m.felice@gmail.com>
2021-12-26 13:52:36 -0500

    Move nexus-tools to pkg/

--- nexus/default.nix
+++ nexus/default.nix
@@ -2,12 +2,6 @@
 
 {
   config = {
-    nixpkgs.overlays = [
-      (self: super: {
-        nexus-tools = super.callPackage ./tools {};
-      })
-    ];
-
     environment.systemPackages = [ pkgs.nexus-tools ];
   };
 }
--- pkg/default.nix
+++ pkg/default.nix
@@ -1,10 +1,13 @@
-{ ... }: {
+{ ... }:
+
+{
   config = {
     nixpkgs.overlays = [
       (self: super: {
         local = (super.local or {}) // {
           git-scripts = super.callPackage ./git-scripts {};
         };
+        nexus-tools = super.callPackage ./nexus-tools {};
         open-in-chrome-tab = super.callPackage ./open-in-chrome-tab {};
       })
     ];
