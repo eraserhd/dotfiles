81c589ac4a4570b9e597cb8e67348405f99688cd
Jason Felice <jason.m.felice@gmail.com>
2021-12-26 14:02:33 -0500

    Remove old overrides for jdk11

--- programs/clojure/default.nix
+++ programs/clojure/default.nix
@@ -3,17 +3,6 @@
 with lib;
 {
   config = {
-    nixpkgs.overlays = [
-      (self: super: {
-        jdk = super.jdk11;
-
-        # fop doesn't like my jdk11 override
-        fop = (super.fop.override {
-          jdk = self.jdk8;
-        });
-      })
-    ];
-
     environment.systemPackages = with pkgs; [
       clojure
       leiningen
