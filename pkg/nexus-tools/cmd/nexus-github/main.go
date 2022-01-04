6e3c1894c0b073be7c7059118bbb425aec33fec0
Jason Felice <jason.m.felice@gmail.com>
2022-01-04 18:55:20 -0500

    Use quilt driver in main

--- pkg/nexus-tools/cmd/nexus-github/main.go
+++ pkg/nexus-tools/cmd/nexus-github/main.go
@@ -8,11 +8,19 @@ import (
 	"github.com/cayleygraph/quad"
 	"github.com/cayleygraph/quad/voc/rdf"
 	"github.com/eraserhd/dotfiles/nexus/tools/pkg/github"
+	_ "github.com/eraserhd/dotfiles/nexus/tools/pkg/quilt"
 )
 
 func main() {
-	g, err := cayley.NewGraph("github", "", map[string]interface{}{
-		"token": os.Getenv("GITHUB_TOKEN"),
+	g, err := cayley.NewGraph("quilt", "", map[string]interface{}{
+		"substores": []map[string]interface{}{
+			{
+				"type": "github",
+				"options": map[string]interface{}{
+					"token": os.Getenv("GITHUB_TOKEN"),
+				},
+			},
+		},
 	})
 	if err != nil {
 		log.Fatalln(err)
