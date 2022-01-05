876b9a7f4e72d4218e6992acd9da9a11ae21e71c
Jason Felice <jason.m.felice@gmail.com>
2022-01-04 19:29:17 -0500

    Implement quilt.Stats

--- pkg/nexus-tools/pkg/quilt/quilt_test.go
+++ pkg/nexus-tools/pkg/quilt/quilt_test.go
@@ -1,10 +1,12 @@
 package quilt
 
 import (
+	"context"
 	"testing"
 
 	"github.com/cayleygraph/cayley"
 	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/quad"
 )
 
 func Test_Can_create_quilt_backend(t *testing.T) {
@@ -24,12 +26,26 @@ func Test_Can_create_quilt_backend(t *testing.T) {
 func Test_aggregates_stats(t *testing.T) {
 	memstore1, err := graph.NewQuadStore("memstore", "", nil)
 	if err != nil {
-		t.Fatalf("want graph.NewQuadStore(\"memstore\", \"\", nil) = nil, got %v", err)
+		t.Fatalf("memstore1: want graph.NewQuadStore(\"memstore\", \"\", nil) = nil, got %v", err)
 	}
+	qw1, err := memstore1.NewQuadWriter()
+	if err != nil {
+		t.Fatalf("want memstore1.NewQuadWriter() to succeed, got %v", err)
+	}
+	qw1.WriteQuad(quad.Make(quad.IRI("<s1>"), quad.IRI("<p1>"), quad.IRI("<o1>"), nil))
+	qw1.Close()
+
 	memstore2, err := graph.NewQuadStore("memstore", "", nil)
 	if err != nil {
 		t.Fatalf("want graph.NewQuadStore(\"memstore\", \"\", nil) = nil, got %v", err)
 	}
+	qw2, err := memstore1.NewQuadWriter()
+	if err != nil {
+		t.Fatalf("want memstore1.NewQuadWriter() to succeed, got %v", err)
+	}
+	qw2.WriteQuad(quad.Make(quad.IRI("<s1>"), quad.IRI("<p1>"), quad.IRI("<o1>"), nil))
+	qw2.WriteQuad(quad.Make(quad.IRI("<s1>"), quad.IRI("<p1>"), quad.IRI("<o2>"), nil))
+	qw2.Close()
 
 	qs, err := cayley.NewGraph("quilt", "", map[string]interface{}{
 		"substores": []graph.QuadStore{
@@ -41,5 +57,13 @@ func Test_aggregates_stats(t *testing.T) {
 		t.Errorf("want err = nil, got %v", err)
 	}
 	defer qs.Close()
+
+	stats, err := qs.Stats(context.TODO(), false)
+	if stats.Nodes.Size != 4 {
+		t.Errorf("want stats.Node.Size = 4, got %d", stats.Nodes.Size)
+	}
+	if stats.Quads.Size != 2 {
+		t.Errorf("want stats.Quads.Size = 2, got %d", stats.Quads.Size)
+	}
 
 }
