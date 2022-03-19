9c65968ec1c0b48252e67185d65a0bf5a5e88d34
Jason Felice <jason.m.felice@gmail.com>
2022-03-19 15:47:29 -0400

    Configurable domain

--- networking/ses-email/default.nix
+++ networking/ses-email/default.nix
@@ -16,7 +16,7 @@ in {
       useTLS = true;
       useSTARTTLS = true;
       hostName = "email-smtp.us-west-2.amazonaws.com:587";
-      domain = "${config.networking.hostName}.eraserhead.net";
+      domain = "${config.networking.hostName}.${config.networking.domain}";
       root = "jason.m.felice@gmail.com";
       authUser = "AKIATJ6VYKJDVEPD7C75";
       authPassFile = toString ./password;
