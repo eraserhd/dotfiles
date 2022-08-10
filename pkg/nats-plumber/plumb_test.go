f5b11902e82aaa9a81685dd69cf8e38b0d3319bf
Jason Felice <jason.m.felice@gmail.com>
2022-08-10 10:28:18 -0400

    test parse error

--- pkg/nats-plumber/plumb_test.go
+++ pkg/nats-plumber/plumb_test.go
@@ -1,43 +1,50 @@
 package nats_plumber
 
 import (
-        "fmt"
-        "reflect"
-        "testing"
+	"fmt"
+	"reflect"
+	"testing"
 )
 
 func TestParseAttributes(t *testing.T) {
-        for _, testCase := range []struct{
-                input string
-                output map[string]string
-        }{
-                {
-                        input: "",
-                        output: map[string]string{},
-                },
-                {
-                        input: "foo=bar baz=quux",
-                        output: map[string]string{
-                                "foo": "bar",
-                                "baz": "quux",
-                        },
-                },
-                {
-                        input: "foo='bar' ba'z=qu'ux",
-                        output: map[string]string{
-                                "foo": "bar",
-                                "baz": "quux",
-                        },
-                },
-        } {
-                t.Run(fmt.Sprintf("parsing %q", testCase.input), func (t *testing.T) {
-                        out, err := ParseAttributes(testCase.input)
-                        if err != nil {
-                                t.Errorf("parse error: %v", err)
-                        }
-                        if !reflect.DeepEqual(testCase.output, out) {
-                                t.Errorf("wrong output.  Expected:\n%v\nGot:\n%v", testCase.output, out)
-                        }
-                })
-        }
+	for _, testCase := range []struct {
+		input  string
+		output map[string]string
+		err    error
+	}{
+		{
+			input:  "",
+			output: map[string]string{},
+		},
+		{
+			input: "foo=bar baz=quux",
+			output: map[string]string{
+				"foo": "bar",
+				"baz": "quux",
+			},
+		},
+		{
+			input: "foo='bar' ba'z=qu'ux",
+			output: map[string]string{
+				"foo": "bar",
+				"baz": "quux",
+			},
+		},
+		{
+			input: "foobar",
+			err:   NoEqualsError,
+		},
+	} {
+		t.Run(fmt.Sprintf("parsing %q", testCase.input), func(t *testing.T) {
+			out, err := ParseAttributes(testCase.input)
+			if err != testCase.err {
+				t.Errorf("unexpected error state: %v", err)
+			}
+			if testCase.output != nil {
+				if !reflect.DeepEqual(testCase.output, out) {
+					t.Errorf("wrong output.  Expected:\n%v\nGot:\n%v", testCase.output, out)
+				}
+			}
+		})
+	}
 }
