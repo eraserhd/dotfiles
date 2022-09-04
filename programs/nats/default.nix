cc3427e50d8ea26e62030e13465c0e736c7be1e6
Jason Felice <jason.m.felice@gmail.com>
2022-09-04 16:08:42 -0400

    nats running in k3s!

--- programs/nats/default.nix
+++ programs/nats/default.nix
@@ -3,12 +3,28 @@
 
 let
   nats-manifest = pkgs.writeText "test.yaml" ''
-    apiVersion: v1
-    kind: ConfigMap
+    apiVersion: helm.cattle.io/v1
+    kind: HelmChart
     metadata:
-      name: a-test
-    data:
-      foo: "bar"
+      name: nats
+      namespace: kube-system
+    spec:
+      repo: https://nats-io.github.io/k8s/helm/charts/
+      chart: nats
+      targetNamespace: default
+      valuesContent: |-
+        nats:
+          image: nats:2.8.4-alpine
+          jetstream:
+            enabled: true
+          #externalAccess: true
+        memStorage:
+          enabled: true
+          size: 128Mi
+        fileStorage:
+          enabled: true
+          size: 128Mi
+          storageDiretory: /data/
   '';
 in {
   config = {
