ddf03ea59c8e26e995338219ebae3cbbd0994cbf
Jason Felice <jason.m.felice@gmail.com>
2022-09-10 17:35:48 -0400

    Re-enable open-in-chrome-tab, fix warning

--- browser/default.nix
+++ browser/default.nix
@@ -12,30 +12,30 @@ with lib;
     local.openURLsInChrome = mkEnableOption "Open URLs in Chrome";
   };
 
-  #config = mkIf config.local.openURLsInChrome
-  #  (if (builtins.hasAttr "launchd" options)
-  #   then {
-  #     launchd.agents.open-in-chrome-tab = {
-  #       path = with pkgs; [
-  #         natscli
-  #         open-in-chrome-tab
-  #       ];
-  #       script = ''
-  #         export PATH="$PATH":/usr/bin
-  #         nats sub -r browser.open |while read -r url; do
-  #           open-in-chrome-tab "$url"
-  #         done
-  #       '';
-  #       serviceConfig = {
-  #         KeepAlive = true;
-  #       };
-  #     };
-  #     environment.systemPackages = [ pkgs.open-in-chrome-tab ];
-  #   }
-  #   else {
-  #     assertions = [{
-  #       assertion = false;
-  #       message = "local.openURLsInChrome is not available on NixOS yet";
-  #     }];
-  #   });
+  config = mkIf config.local.openURLsInChrome
+    (if (builtins.hasAttr "launchd" options)
+     then {
+       launchd.user.agents.open-in-chrome-tab = {
+         path = with pkgs; [
+           natscli
+           open-in-chrome-tab
+         ];
+         script = ''
+           export PATH="$PATH":/usr/bin
+           nats sub -r browser.open |while read -r url; do
+             open-in-chrome-tab "$url"
+           done
+         '';
+         serviceConfig = {
+           KeepAlive = true;
+         };
+       };
+       environment.systemPackages = [ pkgs.open-in-chrome-tab ];
+     }
+     else {
+       assertions = [{
+         assertion = false;
+         message = "local.openURLsInChrome is not available on NixOS yet";
+       }];
+     });
 }
