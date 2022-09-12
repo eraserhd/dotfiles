08134ffacdf665ea906685fd744b65e171027fbe
Jason Felice <jason.m.felice@gmail.com>
2022-09-12 07:37:11 -0400

    Most basic plumber

--- pkg/nats-plumber/cmd/plumber/plumber.go
+++ pkg/nats-plumber/cmd/plumber/plumber.go
@@ -11,16 +11,28 @@ func main() {
 	if err != nil {
 		log.Fatalf("connecting to NATS: %v", err)
 	}
-	conn, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer conn.Close()
+	defer nc.Close()
 
-	//recvChan := make(chan *nats_plumber.Message)
-	//if _, err := conn.BindRecvChan("plumb", recvChan); err != nil {
-	//	log.Fatalf("error binding receive channel: %v", err)
-	//}
+	ch := make(chan *nats.Msg)
+	sub, err := nc.ChanSubscribe("plumb.click", ch)
+	if err != nil {
+		log.Fatalf("error binding receive channel: %v", err)
+	}
+	defer sub.Drain()
 
-	//for {
-	//	msg := <-recvChan
-	//	log.Printf("got: %v", msg)
-	//}
+	for {
+		msg := <-ch
+		log.Printf("received %q", string(msg.Data))
+
+		next := nats.NewMsg("browser.open")
+		next.Reply = msg.Reply
+		next.Header = msg.Header
+		next.Data = msg.Data
+		if err := nc.PublishMsg(next); err != nil {
+			log.Printf("error publishing: %v", err)
+			reply := nats.NewMsg(msg.Reply)
+			reply.Header.Add("Status", err.Error())
+			nc.PublishMsg(reply)
+		}
+	}
 }
