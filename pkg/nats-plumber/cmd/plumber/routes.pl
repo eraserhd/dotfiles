d4bb6aa827162d4040d4ee2300f689238d8067f9
Jason Felice <jason.m.felice@gmail.com>
2022-09-17 08:14:59 -0400

    refactor

--- pkg/nats-plumber/cmd/plumber/routes.pl
+++ pkg/nats-plumber/cmd/plumber/routes.pl
@@ -1,17 +1,18 @@
 
+remainder(List,List,[]).
 
 browser_schema --> "http".
 browser_schema --> "https".
-anything([C|Cs]) --> [C], anything(Cs).
-anything("") --> "".
-browser_url --> browser_schema, "://", anything(_).
+browser_url --> browser_schema, "://", remainder(_).
 
-working_dir_host([C|Cs]) --> [C], { [C] \= "/" }, working_dir_host(Cs).
+click_data('browser.open') --> browser_url.
+
+working_dir_host([C|Cs]) --> \+ "/", [C], working_dir_host(Cs).
 working_dir_host("") --> "".
-working_dir_path(P) --> anything(P).
+working_dir_path(P) --> remainder(P).
 working_dir(Host,Path) --> "file://", working_dir_host(Host), working_dir_path(Path).
 
-send(msg('browser.open',Url,Headers)) :-
+send(msg(Dst,Url,Headers)) :-
   plumb(msg('plumb.click',Url,Headers)),
   atom_chars(Url,Chars),
-  phrase(browser_url,Chars).
+  phrase(click_data(Dst),Chars).
