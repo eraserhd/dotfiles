c7dd7a7947e57abe75e67764059fb3d2097f02e1
Jason Felice <jason.m.felice@gmail.com>
2022-09-18 07:46:33 -0400

    Add claims to test

--- pkg/nats-plumber/plumb_test.go
+++ pkg/nats-plumber/plumb_test.go
@@ -8,15 +8,18 @@ import (
 
 func TestParseAttributes(t *testing.T) {
 	for _, testCase := range []struct {
+        	claim string
 		input  string
 		output map[string]string
 		err    error
 	}{
 		{
+        		claim: "an empty string contains no attributes",
 			input:  "",
 			output: map[string]string{},
 		},
 		{
+        		claim: "attributes are separated by whitespace",
 			input: "foo=bar baz=quux",
 			output: map[string]string{
 				"foo": "bar",
@@ -24,6 +27,7 @@ func TestParseAttributes(t *testing.T) {
 			},
 		},
 		{
+        		claim: "parts of attributes can be single-quoted",
 			input: "foo='bar' ba'z=qu'ux",
 			output: map[string]string{
 				"foo": "bar",
@@ -31,26 +35,31 @@ func TestParseAttributes(t *testing.T) {
 			},
 		},
 		{
+        		claim: "every attribute must have an '='",
 			input: "foobar",
 			err:   NoEquals,
 		},
 		{
+        		claim: "unbalanced quotes are an error",
 			input: "foo='",
 			err:   UnterminatedQuote,
 		},
 		{
+        		claim: "an empty string can be single-quoted",
 			input: "foo=''",
 			output: map[string]string{
 				"foo": "",
 			},
 		},
 		{
+        		claim: "doubled single quotes represent a literal single quote",
 			input: "foo=''''",
 			output: map[string]string{
 				"foo": "'",
 			},
 		},
 		{
+        		claim: "???",
 			input: "\tfo''o='hello'' world'!\t  bar'='''quux   ",
 			output: map[string]string{
 				"foo": "hello' world!",
@@ -58,7 +67,7 @@ func TestParseAttributes(t *testing.T) {
 			},
 		},
 	} {
-		t.Run(fmt.Sprintf("parsing %q", testCase.input), func(t *testing.T) {
+		t.Run(fmt.Sprintf(testCase.claim, testCase.input), func(t *testing.T) {
 			out, err := ParseAttributes(testCase.input)
 			if err != testCase.err {
 				t.Errorf("unexpected error state: %v", err)
