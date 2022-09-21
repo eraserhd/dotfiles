d05cd1b84f3d50cbdd7e782c0e383749d0baf345
Jason Felice <jason.m.felice@gmail.com>
2022-09-21 16:26:42 -0400

    Bump nats-plumber

--- dogfood/nats-plumber.nix
+++ dogfood/nats-plumber.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "nats-plumber";
-  rev = "a83ce91a95f0b4fb229582e986f4fb31dc36d597";
-  sha256 = "lKE6Ic1+Ax01zOcps1yL1DKrmkmNJ0zCqIOOyVuXnJo=";
+  rev = "9323bfe0bbfdb12008a9a121a19abaa5dade648c";
+  sha256 = "l1JVKyiAJpjj0pWC/aIMhGSqMY7Z6mlUiayd8ehtRcc=";
 }
