3d14d5ad93987332fccddfc3edf6f0d98c6b0c4c
Jason Felice <jason.m.felice@gmail.com>
2022-09-25 11:07:19 -0400

    Fix daemon

--- dogfood/nats-editor-kak.nix
+++ dogfood/nats-editor-kak.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "nats-editor-kak";
-  rev = "7253b1726e9e4d477498e60f478ac181d1df3409";
-  sha256 = "smwBD933LBp0IhWvNkwTpX6m/wDK7wYb++mA5b3rL00=";
+  rev = "678d7dafd945d3723a0741bcef85febe1842285d";
+  sha256 = "P1Zif4hW4F6iPrCCJZv5c9EwaSQiu53eRo0HlGLqjhk=";
 }
