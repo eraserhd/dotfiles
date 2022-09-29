29c71faac8c5eff2917d4b5092f72f79ad5d2bdd
Jason Felice <jason.m.felice@gmail.com>
2022-09-29 10:59:01 -0400

    Bump plumb for wdir fix

--- dogfood/nats-plumber.nix
+++ dogfood/nats-plumber.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "nats-plumber";
-  rev = "81e759a7436c2e9e9f2f1d2d02e3c78c5a56f334";
-  sha256 = "i7z3HNk8BZy4UzXm4T3k97OSsG2kpiDsPmU33KTBdB0=";
+  rev = "59ea8f41c027fab3339ca0ed92048971741a60e0";
+  sha256 = "c3UvV5XFFkKKhavXPD+PtnC0sCdRElqeWjrzRAEGyEo=";
 }
