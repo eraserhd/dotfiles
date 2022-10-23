6d303be87aace201d0e814b173ae63393a2e4ab4
Jason Felice <jason.m.felice@gmail.com>
2022-10-23 14:31:28 -0400

    Fix dogfood, bump kakoune

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "24d6072353f7c7e7cac84b4eb085036a8c852f96";
-  sha256 = "26ihOsI/RGdmACrVLB/7XM/GvfKSA2iaqKSJeEZEsC8=";
+  rev = "cb33918fa2347746d462689bfbeb683392bfc896";
+  sha256 = "Gc/DiGJf1Tq23hi/+uG8W4bBLijGJT1dYsChYQetsNw=";
 }
