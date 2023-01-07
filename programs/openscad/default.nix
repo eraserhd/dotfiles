ef404b4941dc6dc39da54c3c7e32fd145fe2439d
Jason Felice <jason.m.felice@gmail.com>
2023-01-07 09:15:05 -0500

    Wrap openscad script

--- programs/openscad/default.nix
+++ programs/openscad/default.nix
@@ -3,11 +3,18 @@
 with lib;
 let
   cfg = config.local.kits._3d-printing;
+
 in {
   config = mkIf cfg.enable
     (if (builtins.hasAttr "homebrew" options)
      then {
        homebrew.casks = [ "openscad" ];
+       environment.systemPackages = let
+         openscad-wrapper = pkgs.writeScriptBin "openscad" ''
+           readonly OPENSCAD=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
+           exec "$OPENSCAD" "$@"
+         '';
+       in [ openscad-wrapper ];
      }
      else {
        environment.systemPackages = with pkgs; [ openscad ];
