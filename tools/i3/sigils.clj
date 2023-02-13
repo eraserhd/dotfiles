3da48adb21074b67de23937c56de97d9d34dbd25
Jason Felice <jason.m.felice@gmail.com>
2023-02-13 09:19:05 -0500

    Refactor

--- tools/i3/sigils.clj
+++ tools/i3/sigils.clj
@@ -3,9 +3,20 @@
 (def sigils
   ["a" "b" "c" "d" "e" "f" "g" "i" "m" "n" "o" "p" "q" "r" "s" "t" "u"
    "v" "w" "x" "y" "z"])
+(def directions
+  {"h" "left"
+   "j" "down"
+   "k" "up"
+   "l" "right"})
+
+(defn i3-msg [& args]
+  (let [{:keys [out err exit]} (apply shell/sh "i3-msg" args)]
+    (when-not (zero? exit)
+      (throw (ex-info (str "non-zero exit: " exit "\n" err))))
+    out))
 
 (defn tree []
-  (json/parse-string (:out (shell/sh "i3-msg" "-t" "get_tree")) keyword))
+  (json/parse-string (i3-msg "-t" "get_tree")) keyword)
 (alter-var-root #'tree memoize)
 
 (defn label? [node]
@@ -28,9 +39,8 @@
   (let [labels       (labels)
         interpolated (str/replace command
                                   #"\$\((.)\)"
-                                  (fn [[_ sigil]]
-                                    (str (get labels sigil))))]
-    (shell/sh "i3-msg" interpolated)))
+                                  (comp str labels second))]
+    (i3-msg interpolated)))
 
 (defn relabel []
   (let [commands (map (fn [[sigil window-id]]
@@ -38,7 +48,7 @@
                                 window-id
                                 sigil))
                       (labels))]
-    (shell/sh "i3-msg" (str/join " ; " commands))))
+    (i3-msg (str/join " ; " commands))))
 
 (case (first *command-line-args*)
   "i3-exec" (i3-exec (second *command-line-args*))
