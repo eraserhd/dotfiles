f32f815a5040597f2c3598360928a6f99a7fb7bd
Jason Felice <jason.m.felice@gmail.com>
2023-10-30 11:12:59 -0400

    Bump flakes!

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "eraserhd's machine configs";
 
   inputs = {
-    nixpkgs.url = "github:eraserhd/nixpkgs/kak-lsp-CoreServices-fix";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     darwin.url = "github:LnL7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/master";
