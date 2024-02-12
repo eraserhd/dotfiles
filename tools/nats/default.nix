b82c0ceccbb13cc8ddda3874a478474065a172c1
Jason Felice <jason.m.felice@gmail.com>
2024-02-12 15:52:44 -0500

    Token

--- tools/nats/default.nix
+++ tools/nats/default.nix
@@ -4,6 +4,9 @@ with lib;
 let
   enable = config.local.kits.workstation.enable;
   isBrain = config.local.kits.brain.enable;
+
+  token = builtins.readFile ./token.txt;
+
 in {
   config = mkIf enable {
     environment.systemPackages = with pkgs; [
@@ -13,6 +16,9 @@ in {
       enable = true;
       jetstream = true;
       settings = {
+        authorization = {
+          inherit token;
+        };
         jetstream = {
           max_memory_store = 128 * 1024 * 1024;
           max_file_store = 128 * 1024 * 1024;
@@ -20,10 +26,34 @@ in {
         leafnodes = {
           port = mkIf isBrain 7422;
           remotes = mkIf (!isBrain) [{
-            url = "tls://crunch.eraserhead.net";
+            url = "tls://${token}@crunch.eraserhead.net";
           }];
         };
       };
+    };
+
+    home-manager.users.jfelice = { pkgs, ... }: {
+      home.file.".config/nats/context.txt".text = "plugbench";
+      home.file.".config/nats/context/plugbench.json".text = ''
+        {
+          "description": "Plugbench",
+          "url": "nats://127.0.0.1:4222",
+          "token": "${token}",
+          "user": "",
+          "password": "",
+          "creds": "",
+          "nkey": "",
+          "cert": "",
+          "key": "",
+          "ca": "",
+          "nsc": "",
+          "jetstream_domain": "",
+          "jetstream_api_prefix": "",
+          "jetstream_event_prefix": "",
+          "inbox_prefix": "",
+          "user_jwt": ""
+        }
+      '';
     };
   };
 }
