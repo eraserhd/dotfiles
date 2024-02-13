9a1c4450bd3e02b2a95638456bfc0179dc4dea1e
Jason Felice <jason.m.felice@gmail.com>
2024-02-13 09:08:05 -0500

    Start of NATS token suppot

--- kits/workstation/default.nix
+++ kits/workstation/default.nix
@@ -9,6 +9,8 @@ in {
   };
 
   config = mkIf cfg.enable {
+    plugbench.token = config.services.nats.settings.authorization.token;
+
     plugbench.clipboard.enable = true;
     plugbench.kakoune.enable = true;
   };
