bf7e78c1cbeaa5fb4c2fc5968ce08581c0c8bc0f
Jason Felice <jason.m.felice@gmail.com>
2024-04-02 14:45:23 -0400

    Bump kakoune

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "f6762724ea1c747e803c3ed8269b35dac64b41a9";
-  hash = "sha256-g68lF23IK/zIV2Y5Wxwku3J56F4COwtaySMqVz3M1UM=";
+  rev = "e34735a35041c19cc80d24cab6237abd447b8924";
+  hash = "sha256-Q2hhS6IZ9r5hDPva1R9wVU+dgJYW75Ax1HXpewDju88=";
 }
