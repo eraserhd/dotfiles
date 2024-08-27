ea1a5c2cdbd02419f0d7991788814640b152560c
Jason Felice <jason.m.felice@gmail.com>
2024-08-27 14:19:07 -0400

    For granted

--- tools/aws/default.nix
+++ tools/aws/default.nix
@@ -4,8 +4,6 @@
   config = {
     home-manager.users.jfelice = { pkgs, ... }: {
       home.file.".boto".source = ./boto;
-      home.file.".aws/config".source = ./config;
-      home.file.".aws/credentials.managed".source = ./credentials;
     };
   };
 }
