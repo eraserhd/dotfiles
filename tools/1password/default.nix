12c95ab5e456c3f62a14f229087d099b1887d32f
Jason Felice <jason.m.felice@gmail.com>
2024-10-28 15:34:53 -0400

    Use _1password-cli

--- tools/1password/default.nix
+++ tools/1password/default.nix
@@ -5,7 +5,7 @@ with lib;
   config = mkMerge [
     {
       nixpkgs.config.allowUnfree = true;
-      environment.systemPackages = with pkgs; [ _1password ];
+      environment.systemPackages = with pkgs; [ _1password-cli ];
     }
     (mkIf config.local.kits.workstation.enable {
       homebrew.casks = mkIf pkgs.stdenv.isDarwin [ "1password" ];
