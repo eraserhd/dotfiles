9ec3207bfa6abc27482ec5c27cbce48cf9e8d41c
Jason Felice <jason.m.felice@gmail.com>
2024-11-17 11:28:27 -0500

    Simplify

--- os/nix-darwin/default.nix
+++ os/nix-darwin/default.nix
@@ -5,22 +5,15 @@
     ./modules
   ];
 
-  config = (if (builtins.hasAttr "launchd" options)
-  then {
-    system.defaults.finder.AppleShowAllExtensions = true;
-    system.defaults.finder._FXShowPosixPathInTitle = true;
-    system.defaults.magicmouse.MouseButtonMode = "TwoButton";
-
-    system.defaults.NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-      AppleKeyboardUIMode = 3;
-      InitialKeyRepeat = 20;
-      KeyRepeat = 1;
-      "com.apple.keyboard.fnState" = true;
-    };
-
-    nix.configureBuildUsers = true;
-  }
-  else {
-  });
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder._FXShowPosixPathInTitle = true;
+  system.defaults.magicmouse.MouseButtonMode = "TwoButton";
+  system.defaults.NSGlobalDomain = {
+    AppleInterfaceStyle = "Dark";
+    AppleKeyboardUIMode = 3;
+    InitialKeyRepeat = 20;
+    KeyRepeat = 1;
+    "com.apple.keyboard.fnState" = true;
+  };
+  nix.configureBuildUsers = true;
 }
