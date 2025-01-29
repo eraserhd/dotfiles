e5cf09b6cfe95fcd25fb2221dd9c22dc3d2903f9
Jason Felice <jason.m.felice@gmail.com>
2025-01-29 12:10:09 -0500

    Bump parifer-rust

--- dogfood/parinfer-rust.nix
+++ dogfood/parinfer-rust.nix
@@ -1,7 +1,6 @@
 {
   owner = "eraserhd";
   repo = "parinfer-rust";
-  rev = "1d5cc1b67ea01551b5f1151432de4af598f37dab";
-  sha256 = "/RgE13qwx6I6R4h+uYwBqMdu0gvfShfiS4m0TW2ktEU=";
-  fetchSubmodules = true;
+  rev = "e02b0280111f82facbe19c854a374d713d37ac17";
+  hash = "sha256-2oLhsJHT4Q8KIa1sRLUaVOrDisnG0IZPHvLTqHe7Gcg=";
 }
