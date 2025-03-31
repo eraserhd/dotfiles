8bfd0003033b3bea99989fdf746db270f3b8bfaa
Jason Felice <jason.m.felice@gmail.com>
2025-03-31 19:38:12 -0400

    Use local fork for now

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "c7d688f578c7b58989fc04e7bb1e9b5a939a5730";
-  hash = "sha256-JIqhzigex0YR+ll6OpgP176EFu4F49G/ZDp5ek3Y/hQ=";
+  rev = "a75c693cf1eb1b51862411a70475c2e84abc42a8";
+  hash = "sha256-zcuKNuxQdLyYtjyronf+iyKpHYxK5V/drNkiSmjRZsQ=";
 }
