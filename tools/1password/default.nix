00ccbdbdb7dda5ff8bc3116af30dbf8061c04313
Jason Felice <jason.m.felice@gmail.com>
2025-04-08 14:03:51 -0400

    Install 1password GUI

--- tools/1password/default.nix
+++ tools/1password/default.nix
@@ -5,11 +5,10 @@ with lib;
   config = mkMerge [
     {
       nixpkgs.config.allowUnfree = true;
-      environment.systemPackages = with pkgs; [ _1password-cli ];
+      environment.systemPackages = with pkgs; [
+        _1password-cli
+        _1password-gui
+      ];
     }
-    (mkIf config.local.kits.workstation.enable {
-      homebrew.casks = mkIf pkgs.stdenv.isDarwin [ "1password" ];
-      environment.systemPackages = mkIf (!pkgs.stdenv.isDarwin) [ pkgs._1password-gui ];
-    })
   ];
 }
