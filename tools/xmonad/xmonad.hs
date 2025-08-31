2aca64509d8bd2897a0e7450c7023dc5a5a2a82e
Jason Felice <jason.m.felice@gmail.com>
2025-08-31 18:48:39 -0400

    XMonad h/j/k/l ok

--- tools/xmonad/xmonad.hs
+++ tools/xmonad/xmonad.hs
@@ -1,7 +1,21 @@
 import XMonad
+import XMonad.Actions.Navigation2D
 import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
+
+devLayout = Mirror $ Tall 3 (3/100) (3/4)
+
+myLayout = devLayout ||| Full
 
 main :: IO ()
-main = xmonad $ def
-  { modMask = mod4Mask
+main = xmonad $ withNavigation2DConfig def $ def
+  { modMask = mod4Mask  -- se Command/Super for mod
+  , terminal = "kitty"
+  , layoutHook = myLayout
   }
+ `additionalKeysP`
+  [ ("C-w h", windowGo L False)
+  , ("C-w j", windowGo D False)
+  , ("C-w k", windowGo U False)
+  , ("C-w l", windowGo R False)
+  ]
