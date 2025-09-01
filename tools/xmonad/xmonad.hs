b3810a4b424dd3c9531e71c9e332245ef138a12d
Jason Felice <jason.m.felice@gmail.com>
2025-09-01 17:17:55 -0400

    Shows ?h!!

--- tools/xmonad/xmonad.hs
+++ tools/xmonad/xmonad.hs
@@ -4,11 +4,13 @@ import XMonad
 import XMonad.Actions.FocusNth
 import XMonad.Actions.Navigation2D
 import XMonad.Actions.UpdatePointer
-import XMonad.Layout.Decoration
+import XMonad.Layout.Decoration hiding (Theme)
 import XMonad.Layout.DecorationEx
 import XMonad.Layout.DecorationEx.Common
+import XMonad.Layout.DecorationEx.Engine
 import XMonad.Layout.DecorationEx.Widgets
 import XMonad.Util.EZConfig
+import XMonad.Util.Font
 import XMonad.Util.Paste
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Themes
@@ -36,7 +38,7 @@ myDarkYellow = "#f78c6c"
 myPurple     = "#c792ea"
 myVisualGrey = "#3e4452"
 
-myTheme :: Theme
+-- myTheme :: Theme
 myTheme = def { fontName            = "xft:mononoki-10"
               , activeColor         = myBlack
               , inactiveColor       = myBlack
@@ -48,24 +50,96 @@ myTheme = def { fontName            = "xft:mononoki-10"
               , decoHeight          = 16
               }
 
-data SigilCommand = SigilCommand deriving (Show, Read)
+data SigilEngine widget a = SigilEngine deriving (Show, Read)
 
-instance WindowCommand SigilCommand where
-    executeWindowCommand _ _ = return False
-    isCommandChecked _ _ = return False
+instance (TextWidget widget, ClickHandler (GenericTheme SimpleStyle) widget)
+  => DecorationEngine SigilEngine widget Window where
+  type Theme SigilEngine = GenericTheme SimpleStyle
+  type DecorationPaintingContext SigilEngine = XPaintingContext
+  type DecorationEngineState SigilEngine = XMonadFont
+
+  describeEngine _ = "SigilEngine"
+
+  calcWidgetPlace = calcTextWidgetPlace
+
+  paintWidget = paintTextWidget
+
+  paintDecoration = paintDecorationSimple
+
+  initializeState _ _ theme = initXMF (themeFontName theme)
+  releaseStateResources _ = releaseXMF
+
+-- | Implementation of @paintWidget@ for decoration engines based on @SigilEngine@.
+paintTextWidget :: (TextWidget widget,
+                    Style (Theme engine widget) ~ SimpleStyle,
+                    DecorationPaintingContext engine ~ XPaintingContext,
+                    DecorationEngineState engine ~ XMonadFont,
+                    Shrinker shrinker,
+                    DecorationEngine engine widget Window)
+                => engine widget Window
+                -> DecorationPaintingContext engine
+                -> WidgetPlace
+                -> shrinker
+                -> DrawData engine widget
+                -> widget
+                -> Bool
+                -> X ()
+paintTextWidget engine (dpy, pixmap, gc) place shrinker dd widget _ = do
+    let style = ddStyle dd
+        rect = wpRectangle place
+        x = rect_x rect
+        y = wpTextYPosition place
+    str <- widgetString dd widget
+    str' <- if isShrinkable widget
+              then getShrinkedWindowName engine shrinker (ddEngineState dd) str (rect_width rect) (rect_height rect)
+              else return str
+    printStringXMF dpy pixmap (ddEngineState dd) gc (sTextColor style) (sTextBgColor style) x y str'
+
+-- | Implementation of @calcWidgetPlace@ for decoration engines based on @SigilEngine@.
+calcTextWidgetPlace :: (TextWidget widget,
+                        DecorationEngineState engine ~ XMonadFont,
+                        DecorationEngine engine widget Window)
+                    => engine widget Window
+                    -> DrawData engine widget
+                    -> widget
+                    -> X WidgetPlace
+calcTextWidgetPlace _ dd widget = do
+    str <- widgetString dd widget
+    let h = rect_height (ddDecoRect dd)
+        font = ddEngineState dd
+    withDisplay $ \dpy -> do
+      width <- fi <$> textWidthXMF dpy (ddEngineState dd) str
+      (a, d) <- textExtentsXMF font str
+      let height = a + d
+          y = fi $ (h - fi height) `div` 2
+          y0 = y + fi a
+          rect = Rectangle 0 y width (fi height)
+      return $ WidgetPlace y0 rect
+
+--- END
 
 data SigilWidget = SigilWidget deriving (Show, Read)
 
 instance DecorationWidget SigilWidget where
-    type WidgetCommand SigilWidget = SigilCommand
+    type WidgetCommand SigilWidget = StandardCommand
+
+    widgetCommand _ _ = def
+    isShrinkable _ = False
 
 instance TextWidget SigilWidget where
     widgetString dd _ = return "?h"
 
-myThemeEx = (themeEx myTheme) { exWidgetsLeft = [] -- [SigilWidget]
+myThemeEx :: GenericTheme SimpleStyle SigilWidget
+myThemeEx = (themeEx myTheme) { exWidgetsLeft = [SigilWidget]
                               }
+sigilDecoration :: (Shrinker shrinker)
+                   => shrinker                -- ^ String shrinker, for example @shrinkText@
+                   -> Theme SigilEngine SigilWidget  -- ^ Decoration theme (font, colors, widgets, etc)
+                   -> l Window                -- ^ Layout to be decorated
+                 -> ModifiedLayout (DecorationEx SigilEngine SigilWidget DefaultGeometry shrinker) l Window
+sigilDecoration shrinker theme = decorationEx shrinker theme SigilEngine def
 
-myLayout = textDecoration shrinkText myThemeEx (DevLayout ||| Full)
+myLayout = sigilDecoration shrinkText myThemeEx (DevLayout ||| Full)
 
 sigils = ["a", "b", "c", "d", "e", "g", "i", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
