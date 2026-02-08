5bd16b6cc36696011c94c30fda2132ec529ecb40
Jason Felice <jason.m.felice@gmail.com>
2026-02-08 08:11:41 -0500

    Start tomat daemon, 45/15

--- tools/tomat/default.nix
+++ tools/tomat/default.nix
@@ -3,5 +3,18 @@
 {
   config = {
     environment.systemPackages = [ pkgs.tomat ];
+
+    home-manager.users.jfelice = { pkgs, ... }: {
+      services.tomat = {
+        enable = true;
+
+        settings = {
+          timer = {
+            work = 45;
+            break = 15;
+          };
+        };
+      };
+    };
   };
 }
