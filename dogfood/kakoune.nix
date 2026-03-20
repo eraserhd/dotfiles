585f7f3f983325505a7a282f44541bd81b41a0e0
Jason Felice <jason.m.felice@gmail.com>
2026-03-20 10:19:26 -0400

    Bump kakoune for CPU usage

--- dogfood/kakoune.nix
+++ dogfood/kakoune.nix
@@ -1,6 +1,6 @@
 {
   owner = "eraserhd";
   repo = "kakoune";
-  rev = "db987ffae94a6859d81d86113d7cae8f35eba067";
-  hash = "sha256-t/d9LQiUi1np6ZCYNTGaUJBfQ1UnvQMMDNrcfV7LSX0=";
+  rev = "4d0a484b2d0e96e03c9829d04d649a5feb5d3352";
+  hash = "sha256-7hEMiTi6ZF+5mEO86Po0n1uHzW2RKrscfCc5+nbELT4=";
 }
