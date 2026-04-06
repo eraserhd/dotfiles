978c35188646d21389a9ef5bc4448091f23c23b0
Jason Felice <jason.m.felice@gmail.com>
2026-04-06 14:03:33 -0400

    Restore aws CLI for my account

--- tools/aws/default.nix
+++ tools/aws/default.nix
@@ -2,8 +2,14 @@
 
 {
   config = {
+    environment.systemPackages = [
+      pkgs.awscli2
+    ];
+
     home-manager.users.jfelice = { pkgs, ... }: {
       home.file.".boto".source = ./boto;
+      home.file.".aws/config".source = ./config;
+      home.file.".aws/credentials".source = ./credentials;
     };
   };
 }
