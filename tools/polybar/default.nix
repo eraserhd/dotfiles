59b70aad6595b9038da91f0fb0ba76d0cbee6aa9
Jason Felice <jason.m.felice@gmail.com>
2026-08-31 14:24:41 -0400

    Not for Darwin

--- tools/polybar/default.nix
+++ tools/polybar/default.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, options, ... }:
 
 {
-  config = {
-    environment.systemPackages = [ pkgs.polybar ];
+  config =
+    (if (builtins.hasAttr "launchd" options)
+     then {}
+     else {
+       environment.systemPackages = [ pkgs.polybar ];
 
-    home-manager.users.jfelice = { pkgs, ... }: {
-      home.file.".config/polybar/config.ini".source = ./config.ini;
-    };
-  };
+       home-manager.users.jfelice = { pkgs, ... }: {
+         home.file.".config/polybar/config.ini".source = ./config.ini;
+       };
+     });
 }
