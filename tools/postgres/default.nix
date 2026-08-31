29c05e89b234f5a6a62c251296bb56d209956781
Jason Felice <jason.m.felice@gmail.com>
2026-08-31 14:33:13 -0400

    No longer need this

--- tools/postgres/default.nix
+++ tools/postgres/default.nix
@@ -1,16 +1,4 @@
-{ lib, options, ... }:
+{ pkgs, ... }:
 
-with lib;
 {
-  config = (if (builtins.hasAttr "launchd" options)
-  then {
-    system.activationScripts.extraUserActivation.text = ''
-      rm -f ~/.pgpass
-      cp -ap ${toString ./pgpass.conf} ~/.pgpass
-      chmod 600 ~/.pgpass
-    '';
-  }
-  else {
-    # FIXME: NixOS activation as above
-  });
 }
