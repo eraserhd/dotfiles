7f60d23939fc5cb2ad8b85933832f66fc92e8b12
Jason Felice <jason.m.felice@gmail.com>
2026-08-31 14:55:34 -0400

    Fix deprecated package name

--- tools/tex/default.nix
+++ tools/tex/default.nix
@@ -3,13 +3,7 @@
 {
   config = {
     environment.systemPackages = [
-      pkgs.texlive.combined.scheme-full
-      #(pkgs.texlive.combine {
-      #  inherit (pkgs.texlive)
-      #    scheme-tetex
-      #    collection-fontsextra
-      #    enumitem;
-      #})
+      pkgs.texliveFull
     ];
   };
 }
