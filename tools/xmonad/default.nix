253554c64ea1f7dd1a50b42f507597083c70c1da
Jason Felice <jason.m.felice@gmail.com>
2026-08-31 14:24:34 -0400

    Not for Darwin

--- tools/xmonad/default.nix
+++ tools/xmonad/default.nix
@@ -1,19 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, options, ... }:
 
 {
-  config = {
-    services.xserver.windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
-    };
+  config =
+    (if (builtins.hasAttr "launchd" options)
+     then {}
+     else {
+       services.xserver.windowManager.xmonad = {
+         enable = true;
+         enableContribAndExtras = true;
+       };
 
-    environment.systemPackages = with pkgs; [
-      dmenu
-    ];
+       environment.systemPackages = with pkgs; [
+         dmenu
+       ];
 
-    home-manager.users.jfelice = { pkgs, ... }: {
-      home.file.".config/xmonad/xmonad.hs".text = builtins.readFile ./config/xmonad.hs;
-    };
-
-  };
+       home-manager.users.jfelice = { pkgs, ... }: {
+         home.file.".config/xmonad/xmonad.hs".text = builtins.readFile ./config/xmonad.hs;
+       };
+     });
 }
