003966733d422cdb6a7334baab4fb432378f2ffa
Jason Felice <jason.m.felice@gmail.com>
2026-08-31 14:29:47 -0400

    No qemu on MacOS

--- tools/qemu/default.nix
+++ tools/qemu/default.nix
@@ -1,10 +1,13 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, options, ... }:
 
 with lib;
 {
-  config = mkIf config.local.kits.develop.enable {
-    virtualisation.libvirtd.enable = true;
-    programs.virt-manager.enable = true;
-    users.extraGroups.libvirt.members = [ "jfelice" ];
-  };
+  config = mkIf config.local.kits.develop.enable
+    (if (builtins.hasAttr "launchd" options)
+     then {}
+     else {
+       virtualisation.libvirtd.enable = true;
+       programs.virt-manager.enable = true;
+       users.extraGroups.libvirt.members = [ "jfelice" ];
+     });
 }
